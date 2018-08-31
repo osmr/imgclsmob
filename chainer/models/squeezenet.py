17b9e98a6c7e4d974ccd79267f748a0c52525219
osmr <osemery@gmail.com>
2018-08-31 18:30:21 +0300

    Some experiments with Chainer

---
+++ chainer/models/squeezenet.py
@@ -0,0 +1,351 @@
+"""
+    SqueezeNet, implemented in Chainer.
+    Original paper: 'SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and <0.5MB model size,'
+    https://arxiv.org/abs/1602.07360.
+"""
+
+__all__ = ['SqueezeNet', 'squeezenet_v1_0', 'squeezenet_v1_1', 'squeezeresnet_v1_0', 'squeezeresnet_v1_1']
+
+import os
+import chainer
+import chainer.functions as F
+import chainer.links as L
+
+
+class FireConv(chainer.Chain):
+    """
+    SqueezeNet specific convolution block.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    ksize : int or tuple/list of 2 int
+        Convolution window size.
+    pad : int or tuple/list of 2 int
+        Padding value for convolution layer.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 ksize,
+                 pad):
+        super(FireConv, self).__init__()
+        with self.init_scope():
+            self.conv = L.Convolution2D(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                ksize=ksize,
+                pad=pad)
+
+    def __call__(self, x):
+        x = self.conv(x)
+        x = F.relu(x)
+        return x
+
+
+class FireUnit(chainer.Chain):
+    """
+    SqueezeNet unit, so-called 'Fire' unit.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    squeeze_channels : int
+        Number of output channels for squeeze convolution blocks.
+    expand1x1_channels : int
+        Number of output channels for expand 1x1 convolution blocks.
+    expand3x3_channels : int
+        Number of output channels for expand 3x3 convolution blocks.
+    residual : bool
+        Whether use residual connection.
+    """
+    def __init__(self,
+                 in_channels,
+                 squeeze_channels,
+                 expand1x1_channels,
+                 expand3x3_channels,
+                 residual):
+        super(FireUnit, self).__init__()
+        self.residual = residual
+
+        with self.init_scope():
+            self.squeeze = FireConv(
+                in_channels=in_channels,
+                out_channels=squeeze_channels,
+                ksize=1,
+                pad=0)
+            self.expand1x1 = FireConv(
+                in_channels=squeeze_channels,
+                out_channels=expand1x1_channels,
+                ksize=1,
+                pad=0)
+            self.expand3x3 = FireConv(
+                in_channels=squeeze_channels,
+                out_channels=expand3x3_channels,
+                ksize=3,
+                pad=1)
+
+    def __call__(self, x):
+        if self.residual:
+            identity = x
+        x = self.squeeze(x)
+        y1 = self.expand1x1(x)
+        y2 = self.expand3x3(x)
+        out = F.concat((y1, y2), axis=1)
+        if self.residual:
+            out = out + identity
+        return out
+
+
+class SqueezeInitBlock(chainer.Chain):
+    """
+    SqueezeNet specific initial block.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    ksize : int or tuple/list of 2 int
+        Convolution window size.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 ksize):
+        super(SqueezeInitBlock, self).__init__()
+        with self.init_scope():
+            self.conv = L.Convolution2D(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                ksize=ksize,
+                stride=2)
+
+    def __call__(self, x):
+        x = self.conv(x)
+        x = F.relu(x)
+        return x
+
+
+class SqueezeNet(chainer.Chain):
+    """
+    SqueezeNet model from 'SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and <0.5MB model size,'
+    https://arxiv.org/abs/1602.07360.
+
+    Parameters:
+    ----------
+    channels : list of list of int
+        Number of output channels for each unit.
+    residuals : bool
+        Whether to use residual units.
+    init_block_kernel_size : int or tuple/list of 2 int
+        The dimensions of the convolution window for the initial unit.
+    init_block_channels : int
+        Number of output channels for the initial unit.
+    in_channels : int, default 3
+        Number of input channels.
+    classes : int, default 1000
+        Number of classification classes.
+    """
+    def __init__(self,
+                 channels,
+                 residuals,
+                 init_block_kernel_size,
+                 init_block_channels,
+                 in_channels=3,
+                 classes=1000):
+        super(SqueezeNet, self).__init__()
+
+        with self.init_scope():
+            self.features = L.Sequential()
+            self.features.append(SqueezeInitBlock(
+                in_channels=in_channels,
+                out_channels=init_block_channels,
+                ksize=init_block_kernel_size))
+            in_channels = init_block_channels
+            for i, channels_per_stage in enumerate(channels):
+                stage = L.Sequential()
+                with stage.name_scope():
+                    stage.add(F.max_pooling_2d(
+                        ksize=3,
+                        stride=2))
+                    for j, out_channels in enumerate(channels_per_stage):
+                        expand_channels = out_channels // 2
+                        squeeze_channels = out_channels // 8
+                        stage.append(FireUnit(
+                            in_channels=in_channels,
+                            squeeze_channels=squeeze_channels,
+                            expand1x1_channels=expand_channels,
+                            expand3x3_channels=expand_channels,
+                            residual=((residuals is not None) and (residuals[i][j] == 1))))
+                        in_channels = out_channels
+                self.features.append(stage)
+            self.features.append(L.Dropout(ratio=0.5))
+
+            self.output = L.Sequential()
+            self.output.append(L.Convolution2D(
+                in_channels=in_channels,
+                out_channels=classes,
+                ksize=1))
+            self.output.append(F.relu)
+            self.output.append(L.AveragePooling2D(
+                ksize=13,
+                stride=1))
+            self.output.append(F.flatten)
+
+    def __call__(self, x):
+        x = self.features(x)
+        x = self.output(x)
+        return x
+
+
+def get_squeezenet(version,
+                   residual=False,
+                   model_name=None,
+                   pretrained=False,
+                   **kwargs):
+    """
+    Create SqueezeNet model with specific parameters.
+
+    Parameters:
+    ----------
+    version : str
+        Version of SqueezeNet ('1.0' or '1.1').
+    residual : bool, default False
+        Whether to use residual connections.
+    model_name : str or None, default None
+        Model name for loading pretrained model.
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    """
+
+    if version == '1.0':
+        channels = [[128, 128, 256], [256, 384, 384, 512], [512]]
+        residuals = [[0, 1, 0], [1, 0, 1, 0], [1]]
+        init_block_kernel_size = 7
+        init_block_channels = 96
+    elif version == '1.1':
+        channels = [[128, 128], [256, 256], [384, 384, 512, 512]]
+        residuals = [[0, 1], [0, 1], [0, 1, 0, 1]]
+        init_block_kernel_size = 3
+        init_block_channels = 64
+    else:
+        raise ValueError("Unsupported SqueezeNet version {}".format(version))
+
+    if not residual:
+        residuals = None
+
+    net = SqueezeNet(
+        channels=channels,
+        residuals=residuals,
+        init_block_kernel_size=init_block_kernel_size,
+        init_block_channels=init_block_channels,
+        **kwargs)
+
+    # if pretrained:
+    #     if (model_name is None) or (not model_name):
+    #         raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
+    #     from .model_store import get_model_file
+    #     net.load_parameters(
+    #         filename=get_model_file(
+    #             model_name=model_name,
+    #             local_model_store_dir_path=root),
+    #         ctx=ctx)
+
+    return net
+
+
+def squeezenet_v1_0(**kwargs):
+    """
+    SqueezeNet 'vanilla' model from 'SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and <0.5MB model
+    size,' https://arxiv.org/abs/1602.07360.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.mxnet/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenet(version="1.0", residual=False, model_name="squeezenet_v1_0", **kwargs)
+
+
+def squeezenet_v1_1(**kwargs):
+    """
+    SqueezeNet v1.1 model from 'SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and <0.5MB model
+    size,' https://arxiv.org/abs/1602.07360.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.mxnet/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenet(version="1.1", residual=False, model_name="squeezenet_v1_1", **kwargs)
+
+
+def squeezeresnet_v1_0(**kwargs):
+    """
+    SqueezeNet model with residual connections from 'SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and
+    <0.5MB model size,' https://arxiv.org/abs/1602.07360.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.mxnet/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenet(version="1.0", residual=True, model_name="squeezeresnet_v1_0", **kwargs)
+
+
+def squeezeresnet_v1_1(**kwargs):
+    """
+    SqueezeNet v1.1 model with residual connections from 'SqueezeNet: AlexNet-level accuracy with 50x fewer parameters
+    and <0.5MB model size,' https://arxiv.org/abs/1602.07360.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.mxnet/models'
+        Location for keeping the model parameters.
+    """
+    return get_squeezenet(version="1.1", residual=True, model_name="squeezeresnet_v1_1", **kwargs)
+
+
+def _test():
+
+    pretrained = False
+
+    models = [
+        squeezenet_v1_0,
+        squeezenet_v1_1,
+        # squeezeresnet_v1_0,
+        # squeezeresnet_v1_1,
+    ]
+
+    for model in models:
+
+        net = model(pretrained=pretrained)
+        
+
+
+
+if __name__ == "__main__":
+    _test()
+

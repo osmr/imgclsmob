a236b803f1203289b3ba18e10cd7369c1c4453ac
osmr <osemery@gmail.com>
2018-09-03 13:57:48 +0300

    Add CH-ResNet models into the list

--- chainer_/model_utils.py
+++ chainer_/model_utils.py
@@ -1,3 +1,4 @@
+from .models.resnet import *
 from .models.squeezenet import *
 from .models.mobilenet import *
 
@@ -5,6 +6,36 @@ __all__ = ['get_model']
 
 
 _models = {
+    'resnet10': resnet10,
+    'resnet12': resnet12,
+    'resnet14': resnet14,
+    'resnet16': resnet16,
+    'resnet18_wd4': resnet18_wd4,
+    'resnet18_wd2': resnet18_wd2,
+    'resnet18_w3d4': resnet18_w3d4,
+
+    'resnet18': resnet18,
+    'resnet34': resnet34,
+    'resnet50': resnet50,
+    'resnet50b': resnet50b,
+    'resnet101': resnet101,
+    'resnet101b': resnet101b,
+    'resnet152': resnet152,
+    'resnet152b': resnet152b,
+    'resnet200': resnet200,
+    'resnet200b': resnet200b,
+
+    'seresnet18': seresnet18,
+    'seresnet34': seresnet34,
+    'seresnet50': seresnet50,
+    'seresnet50b': seresnet50b,
+    'seresnet101': seresnet101,
+    'seresnet101b': seresnet101b,
+    'seresnet152': seresnet152,
+    'seresnet152b': seresnet152b,
+    'seresnet200': seresnet200,
+    'seresnet200b': seresnet200b,
+
     'squeezenet_v1_0': squeezenet_v1_0,
     'squeezenet_v1_1': squeezenet_v1_1,
 

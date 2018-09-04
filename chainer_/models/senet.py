ccb9f8c30d2f83df21ec84dd53429b7106ff7498
osmr <osemery@gmail.com>
2018-09-04 13:29:25 +0300

    Add CH SENet script

---
+++ chainer_/models/senet.py
@@ -0,0 +1,401 @@
+"""
+    SENet, implemented in Chainer.
+    Original paper: 'Squeeze-and-Excitation Networks,' https://arxiv.org/abs/1709.01507.
+"""
+
+__all__ = ['SENet', 'senet52', 'senet103', 'senet154']
+
+import os
+import math
+import chainer.functions as F
+import chainer.links as L
+from chainer import Chain
+from functools import partial
+from chainer.serializers import load_npz
+from .common import SimpleSequential, SEBlock
+from .resnext import resnext_conv3x3, resnext_conv1x1
+
+
+class SENetBottleneck(Chain):
+    """
+    SENet bottleneck block for residual path in SENet unit.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int
+        Stride of the convolution.
+    cardinality: int
+        Number of groups.
+    bottleneck_width: int
+        Width of bottleneck block.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride,
+                 cardinality,
+                 bottleneck_width):
+        super(SENetBottleneck, self).__init__()
+        mid_channels = out_channels // 4
+        D = int(math.floor(mid_channels * (bottleneck_width / 64)))
+        group_width = cardinality * D
+        group_width2 = group_width // 2
+
+        with self.init_scope():
+            self.conv1 = resnext_conv1x1(
+                in_channels=in_channels,
+                out_channels=group_width2,
+                stride=1,
+                activate=True)
+            self.conv2 = resnext_conv3x3(
+                in_channels=group_width2,
+                out_channels=group_width,
+                stride=stride,
+                groups=cardinality,
+                activate=True)
+            self.conv3 = resnext_conv1x1(
+                in_channels=group_width,
+                out_channels=out_channels,
+                stride=1,
+                activate=False)
+
+    def __call__(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        return x
+
+
+class SENetUnit(Chain):
+    """
+    SENet unit with residual connection.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int
+        Stride of the convolution.
+    cardinality: int
+        Number of groups.
+    bottleneck_width: int
+        Width of bottleneck block.
+    identity_conv3x3 : bool, default False
+        Whether to use 3x3 convolution in the identity link.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride,
+                 cardinality,
+                 bottleneck_width,
+                 identity_conv3x3):
+        super(SENetUnit, self).__init__()
+        self.use_se = True
+        self.resize_identity = (in_channels != out_channels) or (stride != 1)
+
+        with self.init_scope():
+            self.body = SENetBottleneck(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                stride=stride,
+                cardinality=cardinality,
+                bottleneck_width=bottleneck_width)
+            if self.use_se:
+                self.se = SEBlock(channels=out_channels)
+            if self.resize_identity:
+                if identity_conv3x3:
+                    self.identity_conv = resnext_conv3x3(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        stride=stride,
+                        groups=1,
+                        activate=False)
+                else:
+                    self.identity_conv = resnext_conv1x1(
+                        in_channels=in_channels,
+                        out_channels=out_channels,
+                        stride=stride,
+                        activate=False)
+            self.activ = F.relu
+
+    def __call__(self, x):
+        if self.resize_identity:
+            identity = self.identity_conv(x)
+        else:
+            identity = x
+        x = self.body(x)
+        if self.use_se:
+            x = self.se(x)
+        x = x + identity
+        x = self.activ(x)
+        return x
+
+
+class SEInitBlock(Chain):
+    """
+    SENet specific initial block.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels):
+        super(SEInitBlock, self).__init__()
+        mid_channels = out_channels // 2
+
+        with self.init_scope():
+            self.conv1 = resnext_conv3x3(
+                in_channels=in_channels,
+                out_channels=mid_channels,
+                stride=2,
+                groups=1,
+                activate=True)
+            self.conv2 = resnext_conv3x3(
+                in_channels=mid_channels,
+                out_channels=mid_channels,
+                stride=1,
+                groups=1,
+                activate=True)
+            self.conv3 = resnext_conv3x3(
+                in_channels=mid_channels,
+                out_channels=out_channels,
+                stride=1,
+                groups=1,
+                activate=True)
+            self.pool = partial(
+                F.max_pooling_2d,
+                ksize=3,
+                stride=2,
+                pad=1,
+                cover_all=False)
+
+    def __call__(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.pool(x)
+        return x
+
+
+class SENet(Chain):
+    """
+    SENet model from 'Squeeze-and-Excitation Networks,' https://arxiv.org/abs/1709.01507.
+
+    Parameters:
+    ----------
+    channels : list of list of int
+        Number of output channels for each unit.
+    init_block_channels : int
+        Number of output channels for the initial unit.
+    cardinality: int
+        Number of groups.
+    bottleneck_width: int
+        Width of bottleneck block.
+    in_channels : int, default 3
+        Number of input channels.
+    classes : int, default 1000
+        Number of classification classes.
+    """
+    def __init__(self,
+                 channels,
+                 init_block_channels,
+                 cardinality,
+                 bottleneck_width,
+                 in_channels=3,
+                 classes=1000):
+        super(SENet, self).__init__()
+
+        with self.init_scope():
+            self.features = SimpleSequential()
+            with self.features.init_scope():
+                setattr(self.features, "init_block", SEInitBlock(
+                    in_channels=in_channels,
+                    out_channels=init_block_channels))
+                in_channels = init_block_channels
+                for i, channels_per_stage in enumerate(channels):
+                    stage = SimpleSequential()
+                    identity_conv3x3 = (i != 0)
+                    with stage.init_scope():
+                        for j, out_channels in enumerate(channels_per_stage):
+                            stride = 2 if (j == 0) and (i != 0) else 1
+                            setattr(stage, "unit{}".format(j + 1), SENetUnit(
+                                in_channels=in_channels,
+                                out_channels=out_channels,
+                                stride=stride,
+                                cardinality=cardinality,
+                                bottleneck_width=bottleneck_width,
+                                identity_conv3x3=identity_conv3x3))
+                            in_channels = out_channels
+                    setattr(self.features, "stage{}".format(i + 1), stage)
+                setattr(self.features, 'final_pool', partial(
+                    F.average_pooling_2d,
+                    ksize=7,
+                    stride=1))
+
+            self.output = SimpleSequential()
+            with self.output.init_scope():
+                setattr(self.output, 'flatten', partial(
+                    F.reshape,
+                    shape=(-1, in_channels)))
+                setattr(self.output, 'dropout', partial(
+                    F.dropout,
+                    ratio=0.2))
+                setattr(self.output, 'fc', L.Linear(
+                    in_size=in_channels,
+                    out_size=classes))
+
+    def __call__(self, x):
+        x = self.features(x)
+        x = self.output(x)
+        return x
+
+
+def get_senet(blocks,
+              model_name=None,
+              pretrained=False,
+              root=os.path.join('~', '.chainer', 'models'),
+              **kwargs):
+    """
+    Create SENet model with specific parameters.
+
+    Parameters:
+    ----------
+    blocks : int
+        Number of blocks.
+    model_name : str or None, default None
+        Model name for loading pretrained model.
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+
+    if blocks == 52:
+        layers = [3, 4, 6, 3]
+        cardinality = 32
+    elif blocks == 103:
+        layers = [3, 4, 23, 3]
+        cardinality = 32
+    elif blocks == 154:
+        layers = [3, 8, 36, 3]
+        cardinality = 64
+    else:
+        raise ValueError("Unsupported SENet with number of blocks: {}".format(blocks))
+
+    bottleneck_width = 4
+    init_block_channels = 128
+    channels_per_layers = [256, 512, 1024, 2048]
+
+    channels = [[ci] * li for (ci, li) in zip(channels_per_layers, layers)]
+
+    net = SENet(
+        channels=channels,
+        init_block_channels=init_block_channels,
+        cardinality=cardinality,
+        bottleneck_width=bottleneck_width,
+        **kwargs)
+
+    if pretrained:
+        if (model_name is None) or (not model_name):
+            raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
+        from .model_store import get_model_file
+        load_npz(
+            file=get_model_file(
+                model_name=model_name,
+                local_model_store_dir_path=root),
+            obj=net)
+
+    return net
+
+
+def senet52(**kwargs):
+    """
+    SENet-52 model from 'Squeeze-and-Excitation Networks,' https://arxiv.org/abs/1709.01507.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_senet(blocks=52, model_name="senet52", **kwargs)
+
+
+def senet103(**kwargs):
+    """
+    SENet-103 model from 'Squeeze-and-Excitation Networks,' https://arxiv.org/abs/1709.01507.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_senet(blocks=103, model_name="senet103", **kwargs)
+
+
+def senet154(**kwargs):
+    """
+    SENet-154 model from 'Squeeze-and-Excitation Networks,' https://arxiv.org/abs/1709.01507.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_senet(blocks=154, model_name="senet154", **kwargs)
+
+
+def _test():
+    import numpy as np
+    import chainer
+
+    chainer.global_config.train = False
+
+    pretrained = False
+
+    models = [
+        senet52,
+        senet103,
+        senet154,
+    ]
+
+    for model in models:
+
+        net = model(pretrained=pretrained)
+        weight_count = net.count_params()
+        print("m={}, {}".format(model.__name__, weight_count))
+        assert (model != senet52 or weight_count == 44659416)  # 22623272
+        assert (model != senet103 or weight_count == 60963096)  # 38908456
+        assert (model != senet154 or weight_count == 115088984)  # 93018024
+
+        x = np.zeros((1, 3, 224, 224), np.float32)
+        y = net(x)
+        assert (y.shape == (1, 1000))
+
+
+if __name__ == "__main__":
+    _test()
+

eaffd511a60c209b47a7e4c82e5639db0022df15
osmr <osemery@gmail.com>
2018-09-06 04:59:00 +0300

    Working on CH-MENet model

---
+++ chainer_/models/menet.py
@@ -0,0 +1,562 @@
+"""
+    MENet, implemented in Chainer.
+    Original paper: 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile Applications,'
+    https://arxiv.org/abs/1803.09127.
+"""
+
+__all__ = ['MENet', 'menet108_8x1_g3', 'menet128_8x1_g4', 'menet160_8x1_g8', 'menet228_12x1_g3', 'menet256_12x1_g4',
+           'menet348_12x1_g3', 'menet352_12x1_g8', 'menet456_24x1_g3']
+
+import os
+import chainer.functions as F
+import chainer.links as L
+from chainer import Chain
+from functools import partial
+from chainer.serializers import load_npz
+from common import SimpleSequential, conv1x1, ChannelShuffle
+
+
+def depthwise_conv3x3(channels,
+                      stride):
+    """
+    Depthwise convolution 3x3 layer. This is exactly the same layer as in ShuffleNet.
+
+    Parameters:
+    ----------
+    channels : int
+        Number of input/output channels.
+    stride : int or tuple/list of 2 int
+        Stride of the convolution.
+    """
+    return L.Convolution2D(
+        in_channels=channels,
+        out_channels=channels,
+        ksize=3,
+        stride=stride,
+        pad=1,
+        nobias=True,
+        groups=channels)
+
+
+def group_conv1x1(in_channels,
+                  out_channels,
+                  groups):
+    """
+    Group convolution 1x1 layer. This is exactly the same layer as in ShuffleNet.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    groups : int
+        Number of groups.
+    """
+    return L.Convolution2D(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        ksize=1,
+        nobias=True,
+        groups=groups)
+
+
+def conv3x3(in_channels,
+            out_channels,
+            stride):
+    """
+    Convolution 3x3 layer.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int
+        Stride of the convolution.
+    """
+    return L.Convolution2D(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        ksize=3,
+        stride=stride,
+        pad=1,
+        nobias=True)
+
+
+class MEUnit(Chain):
+    """
+    MENet unit.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    side_channels : int
+        Number of side channels.
+    groups : int
+        Number of groups in convolution layers.
+    downsample : bool
+        Whether do downsample.
+    ignore_group : bool
+        Whether ignore group value in the first convolution layer.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 side_channels,
+                 groups,
+                 downsample,
+                 ignore_group):
+        super(MEUnit, self).__init__()
+        self.downsample = downsample
+        mid_channels = out_channels // 4
+
+        if downsample:
+            out_channels -= in_channels
+
+        with self.init_scope():
+            # residual branch
+            self.compress_conv1 = group_conv1x1(
+                in_channels=in_channels,
+                out_channels=mid_channels,
+                groups=(1 if ignore_group else groups))
+            self.compress_bn1 = L.BatchNormalization(size=mid_channels)
+            self.c_shuffle = ChannelShuffle(
+                channels=mid_channels,
+                groups=groups)
+            self.dw_conv2 = depthwise_conv3x3(
+                channels=mid_channels,
+                stride=(2 if self.downsample else 1))
+            self.dw_bn2 = L.BatchNormalization(size=mid_channels)
+            self.expand_conv3 = group_conv1x1(
+                in_channels=mid_channels,
+                out_channels=out_channels,
+                groups=groups)
+            self.expand_bn3 = L.BatchNormalization(size=out_channels)
+            if downsample:
+                self.avgpool = partial(
+                    F.average_pooling_2d,
+                    ksize=3,
+                    stride=2,
+                    pad=1)
+            self.activ = F.relu
+
+            # fusion branch
+            self.s_merge_conv = conv1x1(
+                in_channels=mid_channels,
+                out_channels=side_channels)
+            self.s_merge_bn = L.BatchNormalization(size=side_channels)
+            self.s_conv = conv3x3(
+                in_channels=side_channels,
+                out_channels=side_channels,
+                stride=(2 if self.downsample else 1))
+            self.s_conv_bn = L.BatchNormalization(size=side_channels)
+            self.s_evolve_conv = conv1x1(
+                in_channels=side_channels,
+                out_channels=mid_channels)
+            self.s_evolve_bn = L.BatchNormalization(size=mid_channels)
+
+    def __call__(self, x):
+        identity = x
+        # pointwise group convolution 1
+        x = self.compress_conv1(x)
+        x = self.compress_bn1(x)
+        x = self.activ(x)
+        x = self.c_shuffle(x)
+        # merging
+        y = self.s_merge_conv(x)
+        y = self.s_merge_bn(y)
+        y = self.activ(y)
+        # depthwise convolution (bottleneck)
+        x = self.dw_conv2(x)
+        x = self.dw_bn2(x)
+        # evolution
+        y = self.s_conv(y)
+        y = self.s_conv_bn(y)
+        y = self.activ(y)
+        y = self.s_evolve_conv(y)
+        y = self.s_evolve_bn(y)
+        y = F.sigmoid(y)
+        x = x * y
+        # pointwise group convolution 2
+        x = self.expand_conv3(x)
+        x = self.expand_bn3(x)
+        # identity branch
+        if self.downsample:
+            identity = self.avgpool(identity)
+            x = F.concat((x, identity), axis=1)
+        else:
+            x = x + identity
+        x = self.activ(x)
+        return x
+
+
+class MEInitBlock(Chain):
+    """
+    MENet specific initial block.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels):
+        super(MEInitBlock, self).__init__()
+        with self.init_scope():
+            self.conv = L.Convolution2D(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                ksize=3,
+                stride=2,
+                pad=1,
+                nobias=True)
+            self.bn = L.BatchNormalization(size=out_channels)
+            self.activ = F.relu
+            self.pool = partial(
+                F.max_pooling_2d,
+                ksize=3,
+                stride=2,
+                pad=1,
+                cover_all=False)
+
+    def __call__(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.activ(x)
+        x = self.pool(x)
+        return x
+
+
+class MENet(Chain):
+    """
+    MENet model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile Applications,'
+    https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    channels : list of list of int
+        Number of output channels for each unit.
+    init_block_channels : int
+        Number of output channels for the initial unit.
+    side_channels : int
+        Number of side channels in a ME-unit.
+    groups : int
+        Number of groups in convolution layers.
+    in_channels : int, default 3
+        Number of input channels.
+    classes : int, default 1000
+        Number of classification classes.
+    """
+    def __init__(self,
+                 channels,
+                 init_block_channels,
+                 side_channels,
+                 groups,
+                 in_channels=3,
+                 classes=1000):
+        super(MENet, self).__init__()
+
+        with self.init_scope():
+            self.features = SimpleSequential()
+            with self.features.init_scope():
+                setattr(self.features, "init_block", MEInitBlock(
+                    in_channels=in_channels,
+                    out_channels=init_block_channels))
+                in_channels = init_block_channels
+                for i, channels_per_stage in enumerate(channels):
+                    stage = SimpleSequential()
+                    with stage.init_scope():
+                        for j, out_channels in enumerate(channels_per_stage):
+                            downsample = (j == 0)
+                            ignore_group = (i == 0) and (j == 0)
+                            setattr(stage, "unit{}".format(j + 1), MEUnit(
+                                in_channels=in_channels,
+                                out_channels=out_channels,
+                                side_channels=side_channels,
+                                groups=groups,
+                                downsample=downsample,
+                                ignore_group=ignore_group))
+                            in_channels = out_channels
+                    setattr(self.features, "stage{}".format(i + 1), stage)
+                setattr(self.features, 'final_pool', partial(
+                    F.average_pooling_2d,
+                    ksize=7,
+                    stride=1))
+
+            self.output = SimpleSequential()
+            with self.output.init_scope():
+                setattr(self.output, 'flatten', partial(
+                    F.reshape,
+                    shape=(-1, in_channels)))
+                setattr(self.output, 'fc', L.Linear(
+                    in_size=in_channels,
+                    out_size=classes))
+
+    def __call__(self, x):
+        x = self.features(x)
+        x = self.output(x)
+        return x
+
+
+def get_menet(first_stage_channels,
+              side_channels,
+              groups,
+              model_name=None,
+              pretrained=False,
+              root=os.path.join('~', '.chainer', 'models'),
+              **kwargs):
+    """
+    Create MENet model with specific parameters.
+
+    Parameters:
+    ----------
+    first_stage_channels : int
+        Number of output channels at the first stage.
+    side_channels : int
+        Number of side channels in a ME-unit.
+    groups : int
+        Number of groups in convolution layers.
+    model_name : str or None, default None
+        Model name for loading pretrained model.
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+
+    layers = [4, 8, 4]
+
+    if first_stage_channels == 108:
+        init_block_channels = 12
+        channels_per_layers = [108, 216, 432]
+    elif first_stage_channels == 128:
+        init_block_channels = 12
+        channels_per_layers = [128, 256, 512]
+    elif first_stage_channels == 160:
+        init_block_channels = 16
+        channels_per_layers = [160, 320, 640]
+    elif first_stage_channels == 228:
+        init_block_channels = 24
+        channels_per_layers = [228, 456, 912]
+    elif first_stage_channels == 256:
+        init_block_channels = 24
+        channels_per_layers = [256, 512, 1024]
+    elif first_stage_channels == 348:
+        init_block_channels = 24
+        channels_per_layers = [348, 696, 1392]
+    elif first_stage_channels == 352:
+        init_block_channels = 24
+        channels_per_layers = [352, 704, 1408]
+    elif first_stage_channels == 456:
+        init_block_channels = 48
+        channels_per_layers = [456, 912, 1824]
+    else:
+        raise ValueError("The {} of `first_stage_channels` is not supported".format(first_stage_channels))
+
+    channels = [[ci] * li for (ci, li) in zip(channels_per_layers, layers)]
+
+    net = MENet(
+        channels=channels,
+        init_block_channels=init_block_channels,
+        side_channels=side_channels,
+        groups=groups,
+        **kwargs)
+
+    if pretrained:
+        if (model_name is None) or (not model_name):
+            raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
+        from .model_store import get_model_file
+        load_npz(
+            file=get_model_file(
+                model_name=model_name,
+                local_model_store_dir_path=root),
+            obj=net)
+
+    return net
+
+
+def menet108_8x1_g3(**kwargs):
+    """
+    108-MENet-8x1 (g=3) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=108, side_channels=8, groups=3, model_name="menet108_8x1_g3", **kwargs)
+
+
+def menet128_8x1_g4(**kwargs):
+    """
+    128-MENet-8x1 (g=4) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=128, side_channels=8, groups=4, model_name="menet128_8x1_g4", **kwargs)
+
+
+def menet160_8x1_g8(**kwargs):
+    """
+    160-MENet-8x1 (g=8) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=160, side_channels=8, groups=8, model_name="menet160_8x1_g8", **kwargs)
+
+
+def menet228_12x1_g3(**kwargs):
+    """
+    228-MENet-12x1 (g=3) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=228, side_channels=12, groups=3, model_name="menet228_12x1_g3", **kwargs)
+
+
+def menet256_12x1_g4(**kwargs):
+    """
+    256-MENet-12x1 (g=4) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=256, side_channels=12, groups=4, model_name="menet256_12x1_g4", **kwargs)
+
+
+def menet348_12x1_g3(**kwargs):
+    """
+    348-MENet-12x1 (g=3) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=348, side_channels=12, groups=3, model_name="menet348_12x1_g3", **kwargs)
+
+
+def menet352_12x1_g8(**kwargs):
+    """
+    352-MENet-12x1 (g=8) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=352, side_channels=12, groups=8, model_name="menet352_12x1_g8", **kwargs)
+
+
+def menet456_24x1_g3(**kwargs):
+    """
+    456-MENet-24x1 (g=3) model from 'Merging and Evolution: Improving Convolutional Neural Networks for Mobile
+    Applications,' https://arxiv.org/abs/1803.09127.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    ctx : Context, default CPU
+        The context in which to load the pretrained weights.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_menet(first_stage_channels=456, side_channels=24, groups=3, model_name="menet456_24x1_g3", **kwargs)
+
+
+def _test():
+    import numpy as np
+    import chainer
+
+    chainer.global_config.train = False
+
+    pretrained = False
+
+    models = [
+        menet108_8x1_g3,
+        menet128_8x1_g4,
+        # menet160_8x1_g8,
+        menet228_12x1_g3,
+        menet256_12x1_g4,
+        menet348_12x1_g3,
+        menet352_12x1_g8,
+        menet456_24x1_g3,
+    ]
+
+    for model in models:
+
+        net = model(pretrained=pretrained)
+        weight_count = net.count_params()
+        print("m={}, {}".format(model.__name__, weight_count))
+        assert (model != menet108_8x1_g3 or weight_count == 654516)
+        assert (model != menet128_8x1_g4 or weight_count == 750796)
+        assert (model != menet160_8x1_g8 or weight_count == 850120)
+        assert (model != menet228_12x1_g3 or weight_count == 1806568)
+        assert (model != menet256_12x1_g4 or weight_count == 1888240)
+        assert (model != menet348_12x1_g3 or weight_count == 3368128)
+        assert (model != menet352_12x1_g8 or weight_count == 2272872)
+        assert (model != menet456_24x1_g3 or weight_count == 5304784)
+
+        x = np.zeros((1, 3, 224, 224), np.float32)
+        y = net(x)
+        assert (y.shape == (1, 1000))
+
+
+if __name__ == "__main__":
+    _test()
+

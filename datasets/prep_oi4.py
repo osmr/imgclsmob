3629565df598962382d5e1837b399f6699f84eac
osmr <osemery@gmail.com>
2018-11-16 22:41:54 +0300

    Working on OI4C1K script

--- datasets/prep_oi4.py
+++ datasets/prep_oi4.py
@@ -5,6 +5,7 @@ if __name__ == '__main__' and __package__ is None:
 
 import argparse
 import os
+import cv2
 # import zipfile
 import logging
 import hashlib
@@ -159,6 +160,99 @@ def _download(url, path=None, overwrite=False, sha1_hash=None, retries=5, verify
     return fname
 
 
+def create_train_data_subset(src_dir_path,
+                             dst_dir_path,
+                             src_data_subset_name,
+                             dst_data_subset_name,
+                             annotations_sha1,
+                             urls_sha1):
+    logging.info("Processing <{}> subset...".format(src_data_subset_name))
+
+    dst_data_subset_dir_path = os.path.join(dst_dir_path, dst_data_subset_name)
+    if not os.path.exists(dst_data_subset_dir_path):
+        os.makedirs(dst_data_subset_dir_path)
+
+    annotations_base_url = "https://storage.googleapis.com/openimages/2018_04/{}/".format(src_data_subset_name)
+    annotations_file_name = "{}-annotations-human-imagelabels.csv".format(src_data_subset_name)
+    annotations_url = annotations_base_url + annotations_file_name
+    annotations_file_path = os.path.join(src_dir_path, annotations_file_name)
+    _download(url=annotations_url, path=annotations_file_path, sha1_hash=annotations_sha1)
+
+    urls_base_url = "https://storage.googleapis.com/openimages/2018_04/{}/".format(src_data_subset_name)
+    urls_file_name = "{}-images-with-labels-with-rotation.csv".format(src_data_subset_name)
+    urls_url = urls_base_url + urls_file_name
+    urls_file_path = os.path.join(src_dir_path, urls_file_name)
+    _download(url=urls_url, path=urls_file_path, sha1_hash=urls_sha1)
+
+    ann_df1 = pd.read_csv(annotations_file_path)
+    ann_df1 = ann_df1.query("Confidence == 1")[["ImageID", "LabelName"]]
+
+    ann_df2 = ann_df1.groupby(["LabelName"]).size().reset_index(name="n")
+    ann_df3 = ann_df2.nlargest(1000, "n").sort_values(by="n")
+
+    ann_df4 = ann_df1[ann_df1.LabelName.isin(ann_df3.LabelName)][["ImageID"]].drop_duplicates()
+    ann_df5 = ann_df1[ann_df1.ImageID.isin(ann_df4.ImageID)]
+
+    url_df1 = pd.read_csv(urls_file_path)
+    url_df1 = url_df1[["ImageID", "OriginalURL", "Rotation"]]
+
+    cls_df = pd.DataFrame(columns=["ImageID", "LabelName"])
+
+    for id_ann_df3, row_ann_df3 in ann_df3.iterrows():
+        label_name = row_ann_df3["LabelName"]
+        ann_df7 = ann_df5.query("LabelName == '{}'".format(label_name))[["ImageID"]].drop_duplicates().\
+            sort_values(by="ImageID")
+        assert (len(ann_df7.index) <= row_ann_df3["n"])
+        logging.info("Label <{}> is processed".format(label_name))
+
+        label_dir_name = label_name[3:]
+        label_dir_path = os.path.join(dst_data_subset_dir_path, label_dir_name)
+        if not os.path.exists(label_dir_path):
+            os.makedirs(label_dir_path)
+            logging.info("New label directory is created.")
+
+        image_count = 0
+        for id_ann_df7, row_ann_df7 in ann_df7.iterrows():
+            image_id = row_ann_df7["ImageID"]
+            image_file_path = os.path.join(label_dir_path, image_id + ".jpg")
+            if not os.path.exists(image_file_path):
+                url_df2 = url_df1.query("ImageID == '{}'".format(image_id))
+                assert (len(url_df2.index) == 1)
+                url = url_df2.OriginalURL.values.astype(np.unicode)[0]
+                rot = url_df2.Rotation.values.astype(np.float32)[0]
+                try:
+                    _download(url=url, path=image_file_path)
+                    img = Image.open(image_file_path)
+                    img.verify()
+                    img.close()
+                    if rot != 0.0:
+                        img = cv2.imread(image_file_path)
+                        if rot == 90.0:
+                            img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+                        elif rot == 180.0:
+                            img = cv2.rotate(img, cv2.ROTATE_180)
+                        elif rot == 270.0:
+                            img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                        else:
+                            raise Exception("Wrong rotate angle: {}".format(rot))
+                        cv2.imwrite(image_file_path, img)
+                    cls_df = cls_df.append({"ImageID": image_id, "LabelName": label_name}, ignore_index=True)
+                    image_count = image_count + 1
+                except Exception as err:
+                    logging.warning(err)
+                    if os.path.exists(image_file_path):
+                        os.remove(image_file_path)
+            else:
+                cls_df = cls_df.append({"ImageID": image_id, "LabelName": label_name}, ignore_index=True)
+                image_count = image_count + 1
+            ann_df5 = ann_df5[ann_df5.ImageID != image_id]
+        logging.info("Desired image count = {}, real image count = {}".format(row_ann_df3["n"], image_count))
+
+    cls_list_file_name = "{}-cls.csv".format(src_data_subset_name)
+    cls_list_file_path = os.path.join(dst_dir_path, cls_list_file_name)
+    cls_df.to_csv(cls_list_file_path, index=False)
+
+
 def main():
     args = parse_args()
 
@@ -177,52 +271,21 @@ def main():
     if not os.path.exists(dst_dir_path):
         os.makedirs(dst_dir_path)
 
-    annotations_file_name = "train-annotations-human-imagelabels.csv"
-    annotations_file_path = os.path.join(src_dir_path, annotations_file_name)
-    ann_df1 = pd.read_csv(annotations_file_path)
-    ann_df1 = ann_df1.query("Confidence == 1")[["ImageID", "LabelName"]]
+    create_train_data_subset(
+        src_dir_path=src_dir_path,
+        dst_dir_path=dst_dir_path,
+        src_data_subset_name="train",
+        dst_data_subset_name="train",
+        annotations_sha1="4203637e3fb28f3c57c7d4e0c53121cd5e9e098e",
+        urls_sha1="2f64a7d611426cbc4ac3ffa029908d1871c9317d")
 
-    ann_df2 = ann_df1.groupby(["LabelName"]).size().reset_index(name="n")
-    ann_df3 = ann_df2.nlargest(1000, "n").sort_values(by="n")
-
-    ann_df4 = ann_df1[ann_df1.LabelName.isin(ann_df3.LabelName)][["ImageID"]].drop_duplicates()
-    ann_df5 = ann_df1[ann_df1.ImageID.isin(ann_df4.ImageID)]
-
-    urls_file_name = "train-images-with-labels-with-rotation.csv"
-    urls_file_path = os.path.join(src_dir_path, urls_file_name)
-    url_df1 = pd.read_csv(urls_file_path)
-    url_df1 = url_df1[["ImageID", "OriginalURL", "Rotation"]]
-
-    cls_df6 = pd.DataFrame(columns=["ImageID", "LabelName"])
-
-    for id_df3, row_df3 in ann_df3.iterrows():
-        label_name = row_df3["LabelName"]
-        df7 = ann_df5.query("LabelName == '{}'".format(label_name))[["ImageID"]].drop_duplicates().sort_values(by="ImageID")
-        assert (len(df7.index) <= row_df3["n"])
-        for id_df7, row_df7 in df7.iterrows():
-            image_id = row_df7["ImageID"]
-            url_df2 = url_df1.query("ImageID == '{}'".format(image_id))
-            url = url_df2.OriginalURL.values.astype(np.unicode)[0]
-            rot = url_df2.Rotation.values.astype(np.float32)[0]
-            image_file_path = os.path.join(dst_dir_path, image_id + ".jpg")
-            assert (len(url_df2.index) == 1)
-            try:
-                _download(
-                    url=url,
-                    path=image_file_path)
-                img = Image.open(image_file_path)
-                img.verify()
-                if rot != 0.0:
-                    img.rotate(rot).save(image_file_path)
-                cls_df6 = cls_df6.append({"ImageID": image_id, "LabelName": label_name}, ignore_index=True)
-                ann_df5 = ann_df5[ann_df5.ImageID != image_id]
-            except Exception as err:
-                logging.warning(err)
-                pass
-
-    cls_list_file_name = "train-cls.csv"
-    cls_list_file_path = os.path.join(dst_dir_path, cls_list_file_name)
-    cls_df6.to_csv(cls_list_file_path, index=False)
+    # create_data_subset(
+    #     src_dir_path=src_dir_path,
+    #     dst_dir_path=dst_dir_path,
+    #     src_data_subset_name="validation",
+    #     dst_data_subset_name="val",
+    #     annotations_sha1="1f17e6a50187e7df3d323a56c6557045ed60b284",
+    #     urls_sha1="97af77939cf447fa9a4384972d4ec3b231132972")
 
 
 if __name__ == '__main__':

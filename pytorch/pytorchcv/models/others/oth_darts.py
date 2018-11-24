a721a3454b97f09160dadbdafac703afa7b0113c
osmr <osemery@gmail.com>
2018-11-24 19:27:08 +0300

    Experimenting with DARTS, 2

--- pytorch/pytorchcv/models/others/oth_darts.py
+++ pytorch/pytorchcv/models/others/oth_darts.py
@@ -7,7 +7,8 @@ def drop_path(x, drop_prob):
 
     if drop_prob > 0.:
         keep_prob = 1. - drop_prob
-        mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
+        # mask = Variable(torch.cuda.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
+        mask = Variable(torch.FloatTensor(x.size(0), 1, 1, 1).bernoulli_(keep_prob))
         x.div_(keep_prob)
         x.mul_(mask)
     return x

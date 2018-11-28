ca605aac745d0f6c34370c4f4fbd4fa7c1aed768
osmr <osemery@gmail.com>
2018-11-29 00:08:28 +0300

    Work on CH-AirNeXt

--- chainer_/chainercv2/model_provider.py
+++ chainer_/chainercv2/model_provider.py
@@ -31,6 +31,9 @@ from .models.polynet import *
 from .models.nasnet import *
 from .models.pnasnet import *
 
+from .models.airnet import *
+from .models.airnext import *
+
 __all__ = ['get_model']
 
 
@@ -234,6 +237,14 @@ _models = {
     'nasnet_6a4032': nasnet_6a4032,
 
     'pnasnet5large': pnasnet5large,
+
+    'airnet50_1x64d_r2': airnet50_1x64d_r2,
+    'airnet50_1x64d_r16': airnet50_1x64d_r16,
+    'airnet101_1x64d_r2': airnet101_1x64d_r2,
+
+    'airnext50_32x4d_r2': airnext50_32x4d_r2,
+    'airnext101_32x4d_r2': airnext101_32x4d_r2,
+    'airnext101_32x4d_r16': airnext101_32x4d_r16,
 }
 
 
---
+++ chainer_/chainercv2/models/airnext.py
@@ -0,0 +1,382 @@
+"""
+    AirNeXt, implemented in Chainer.
+    Original paper: 'Attention Inspiring Receptive-Fields Network for Learning Invariant Representations,'
+    https://ieeexplore.ieee.org/document/8510896.
+"""
+
+__all__ = ['AirNeXt', 'airnext50_32x4d_r2', 'airnext101_32x4d_r2', 'airnext101_32x4d_r16']
+
+import os
+import math
+import chainer.functions as F
+import chainer.links as L
+from chainer import Chain
+from functools import partial
+from chainer.serializers import load_npz
+from .common import SimpleSequential
+from .airnet import conv1x1_block, conv3x3_block, AirBlock, AirInitBlock
+
+
+class AirNeXtBottleneck(Chain):
+    """
+    AirNet bottleneck block for residual path in ResNet unit.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int
+        Stride of the convolution.
+    cardinality: int
+        Number of groups.
+    bottleneck_width: int
+        Width of bottleneck block.
+    ratio: int
+        Air compression ratio.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride,
+                 cardinality,
+                 bottleneck_width,
+                 ratio):
+        super(AirNeXtBottleneck, self).__init__()
+        mid_channels = out_channels // 4
+        D = int(math.floor(mid_channels * (bottleneck_width / 64.0)))
+        group_width = cardinality * D
+        self.use_air_block = (stride == 1 and mid_channels < 512)
+
+        with self.init_scope():
+            self.conv1 = conv1x1_block(
+                in_channels=in_channels,
+                out_channels=group_width,
+                stride=1,
+                activate=True)
+            self.conv2 = conv3x3_block(
+                in_channels=group_width,
+                out_channels=group_width,
+                stride=stride,
+                groups=cardinality,
+                activate=True)
+            self.conv3 = conv1x1_block(
+                in_channels=group_width,
+                out_channels=out_channels,
+                stride=1,
+                activate=False)
+            if self.use_air_block:
+                self.air = AirBlock(
+                    in_channels=in_channels,
+                    out_channels=group_width,
+                    groups=(cardinality // ratio),
+                    ratio=ratio)
+
+    def __call__(self, x):
+        if self.use_air_block:
+            att = self.air(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        if self.use_air_block:
+            x = x * att
+        x = self.conv3(x)
+        return x
+
+
+class AirNeXtUnit(Chain):
+    """
+    AirNet unit with residual connection.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    stride : int or tuple/list of 2 int
+        Stride of the convolution.
+    cardinality: int
+        Number of groups.
+    bottleneck_width: int
+        Width of bottleneck block.
+    ratio: int
+        Air compression ratio.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride,
+                 cardinality,
+                 bottleneck_width,
+                 ratio):
+        super(AirNeXtUnit, self).__init__()
+        self.resize_identity = (in_channels != out_channels) or (stride != 1)
+
+        with self.init_scope():
+            self.body = AirNeXtBottleneck(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                stride=stride,
+                cardinality=cardinality,
+                bottleneck_width=bottleneck_width,
+                ratio=ratio)
+            if self.resize_identity:
+                self.identity_conv = conv1x1_block(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    stride=stride,
+                    activate=False)
+            self.activ = F.relu
+
+    def __call__(self, x):
+        if self.resize_identity:
+            identity = self.identity_conv(x)
+        else:
+            identity = x
+        x = self.body(x)
+        x = x + identity
+        x = self.activ(x)
+        return x
+
+
+class AirNeXt(Chain):
+    """
+    AirNet model from 'Attention Inspiring Receptive-Fields Network for Learning Invariant Representations,'
+    https://ieeexplore.ieee.org/document/8510896.
+
+    Parameters:
+    ----------
+    channels : list of list of int
+        Number of output channels for each unit.
+    init_block_channels : int
+        Number of output channels for the initial unit.
+    cardinality: int
+        Number of groups.
+    bottleneck_width: int
+        Width of bottleneck block.
+    ratio: int
+        Air compression ratio.
+    in_channels : int, default 3
+        Number of input channels.
+    in_size : tuple of two ints, default (224, 224)
+        Spatial size of the expected input image.
+    classes : int, default 1000
+        Number of classification classes.
+    """
+    def __init__(self,
+                 channels,
+                 init_block_channels,
+                 cardinality,
+                 bottleneck_width,
+                 ratio,
+                 in_channels=3,
+                 in_size=(224, 224),
+                 classes=1000):
+        super(AirNeXt, self).__init__()
+        self.in_size = in_size
+        self.classes = classes
+
+        with self.init_scope():
+            self.features = SimpleSequential()
+            with self.features.init_scope():
+                setattr(self.features, "init_block", AirInitBlock(
+                    in_channels=in_channels,
+                    out_channels=init_block_channels))
+                in_channels = init_block_channels
+                for i, channels_per_stage in enumerate(channels):
+                    stage = SimpleSequential()
+                    with stage.init_scope():
+                        for j, out_channels in enumerate(channels_per_stage):
+                            stride = 2 if (j == 0) and (i != 0) else 1
+                            setattr(stage, "unit{}".format(j + 1), AirNeXtUnit(
+                                in_channels=in_channels,
+                                out_channels=out_channels,
+                                stride=stride,
+                                cardinality=cardinality,
+                                bottleneck_width=bottleneck_width,
+                                ratio=ratio))
+                            in_channels = out_channels
+                    setattr(self.features, "stage{}".format(i + 1), stage)
+                setattr(self.features, 'final_pool', partial(
+                    F.average_pooling_2d,
+                    ksize=7,
+                    stride=1))
+
+            self.output = SimpleSequential()
+            with self.output.init_scope():
+                setattr(self.output, 'flatten', partial(
+                    F.reshape,
+                    shape=(-1, in_channels)))
+                setattr(self.output, 'fc', L.Linear(
+                    in_size=in_channels,
+                    out_size=classes))
+
+    def __call__(self, x):
+        x = self.features(x)
+        x = self.output(x)
+        return x
+
+
+def get_airnext(blocks,
+                cardinality,
+                bottleneck_width,
+                base_channels,
+                ratio,
+                model_name=None,
+                pretrained=False,
+                root=os.path.join('~', '.chainer', 'models'),
+                **kwargs):
+    """
+    Create AirNet model with specific parameters.
+
+    Parameters:
+    ----------
+    blocks : int
+        Number of blocks.
+    cardinality: int
+        Number of groups.
+    bottleneck_width: int
+        Width of bottleneck block.
+    base_channels: int
+        Base number of channels.
+    ratio: int
+        Air compression ratio.
+    model_name : str or None, default None
+        Model name for loading pretrained model.
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+
+    if blocks == 50:
+        layers = [3, 4, 6, 3]
+    elif blocks == 101:
+        layers = [3, 4, 23, 3]
+    else:
+        raise ValueError("Unsupported AirNeXt with number of blocks: {}".format(blocks))
+
+    bottleneck_expansion = 4
+    init_block_channels = base_channels
+    channels_per_layers = [base_channels * (2 ** i) * bottleneck_expansion for i in range(len(layers))]
+
+    channels = [[ci] * li for (ci, li) in zip(channels_per_layers, layers)]
+
+    net = AirNeXt(
+        channels=channels,
+        init_block_channels=init_block_channels,
+        cardinality=cardinality,
+        bottleneck_width=bottleneck_width,
+        ratio=ratio,
+        **kwargs)
+
+    if pretrained:
+        if (model_name is None) or (not model_name):
+            raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
+        from .model_store import get_model_file
+        load_npz(
+            file=get_model_file(
+                model_name=model_name,
+                local_model_store_dir_path=root),
+            obj=net)
+
+    return net
+
+
+def airnext50_32x4d_r2(**kwargs):
+    """
+    AirNeXt50-32x4d (r=2) model from 'Attention Inspiring Receptive-Fields Network for Learning Invariant
+    Representations,' https://ieeexplore.ieee.org/document/8510896.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_airnext(
+        blocks=50,
+        cardinality=32,
+        bottleneck_width=4,
+        base_channels=64,
+        ratio=2,
+        model_name="airnext50_32x4d_r2",
+        **kwargs)
+
+
+def airnext101_32x4d_r2(**kwargs):
+    """
+    AirNeXt101-32x4d (r=2) model from 'Attention Inspiring Receptive-Fields Network for Learning Invariant
+    Representations,' https://ieeexplore.ieee.org/document/8510896.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_airnext(
+        blocks=101,
+        cardinality=32,
+        bottleneck_width=4,
+        base_channels=64,
+        ratio=2,
+        model_name="airnext101_32x4d_r2",
+        **kwargs)
+
+
+def airnext101_32x4d_r16(**kwargs):
+    """
+    AirNeXt101-32x4d (r=16) model from 'Attention Inspiring Receptive-Fields Network for Learning Invariant
+    Representations,' https://ieeexplore.ieee.org/document/8510896.
+
+    Parameters:
+    ----------
+    pretrained : bool, default False
+        Whether to load the pretrained weights for model.
+    root : str, default '~/.chainer/models'
+        Location for keeping the model parameters.
+    """
+    return get_airnext(
+        blocks=101,
+        cardinality=32,
+        bottleneck_width=4,
+        base_channels=64,
+        ratio=16,
+        model_name="airnext101_32x4d_r16",
+        **kwargs)
+
+
+def _test():
+    import numpy as np
+    import chainer
+
+    chainer.global_config.train = False
+
+    pretrained = False
+
+    models = [
+        airnext50_32x4d_r2,
+        airnext101_32x4d_r2,
+        airnext101_32x4d_r16,
+    ]
+
+    for model in models:
+
+        net = model(pretrained=pretrained)
+        weight_count = net.count_params()
+        print("m={}, {}".format(model.__name__, weight_count))
+        assert (model != airnext50_32x4d_r2 or weight_count == 27604296)
+        assert (model != airnext101_32x4d_r2 or weight_count == 54099272)
+        assert (model != airnext101_32x4d_r16 or weight_count == 45456456)
+
+        x = np.zeros((1, 3, 224, 224), np.float32)
+        y = net(x)
+        assert (y.shape == (1, 1000))
+
+
+if __name__ == "__main__":
+    _test()

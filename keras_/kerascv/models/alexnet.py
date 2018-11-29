72231d83ee5ce0ba97671a9f8fd4607fab6173a6
osmr <osemery@gmail.com>
2018-11-29 21:39:11 +0300

    Work on KE-AlexNet, try to fix a bug, 2

--- keras_/kerascv/models/alexnet.py
+++ keras_/kerascv/models/alexnet.py
@@ -10,7 +10,7 @@ import os
 from keras import backend as K
 from keras import layers as nn
 from keras.models import Model
-from common import conv2d
+from .common import conv2d
 
 
 def alex_conv(x,

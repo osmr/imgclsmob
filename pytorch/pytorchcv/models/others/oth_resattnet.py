eead2fa332cbc84cbdd10215e9a76f1ce87f834e
osmr <osemery@gmail.com>
2018-11-29 17:21:22 +0300

    Start experimenting with ResAttNet

---
+++ pytorch/pytorchcv/models/others/oth_resattnet.py
@@ -0,0 +1,388 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import functools
+from torch.autograd import Variable
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn import init
+import functools
+from torch.autograd import Variable
+import numpy as np
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, input_channels, output_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.stride = stride
+        self.bn1 = nn.BatchNorm2d(input_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(input_channels, output_channels // 4, 1, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(output_channels // 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(output_channels // 4, output_channels // 4, 3, stride, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(output_channels // 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv3 = nn.Conv2d(output_channels // 4, output_channels, 1, 1, bias=False)
+        self.conv4 = nn.Conv2d(input_channels, output_channels, 1, stride, bias=False)
+
+    def forward(self, x):
+        residual = x
+        out = self.bn1(x)
+        out1 = self.relu(out)
+        out = self.conv1(out1)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        if (self.input_channels != self.output_channels) or (self.stride != 1):
+            residual = self.conv4(out1)
+        out += residual
+        return out
+
+
+class AttentionModule_stage1(nn.Module):
+    # input size is 56*56
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 size1=(56, 56),
+                 size2=(28, 28),
+                 size3=(14, 14)):
+        super(AttentionModule_stage1, self).__init__()
+
+        self.first_residual_blocks = ResidualBlock(in_channels, out_channels)
+        self.trunk_branches = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+         )
+
+        self.mpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.softmax1_blocks = ResidualBlock(in_channels, out_channels)
+        self.skip1_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.softmax2_blocks = ResidualBlock(in_channels, out_channels)
+        self.skip2_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool3 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.softmax3_blocks = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+        )
+
+        self.interpolation3 = nn.UpsamplingBilinear2d(size=size3)
+
+        self.softmax4_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.interpolation2 = nn.UpsamplingBilinear2d(size=size2)
+
+        self.softmax5_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.interpolation1 = nn.UpsamplingBilinear2d(size=size1)
+
+        self.softmax6_blocks = nn.Sequential(
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels , kernel_size = 1, stride = 1, bias = False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels , kernel_size = 1, stride = 1, bias = False),
+            nn.Sigmoid()
+        )
+
+        self.last_blocks = ResidualBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        x = self.first_residual_blocks(x)
+        out_trunk = self.trunk_branches(x)
+
+        out_mpool1 = self.mpool1(x)
+        out_softmax1 = self.softmax1_blocks(out_mpool1)
+        out_skip1_connection = self.skip1_connection_residual_block(out_softmax1)
+
+        out_mpool2 = self.mpool2(out_softmax1)
+        out_softmax2 = self.softmax2_blocks(out_mpool2)
+        out_skip2_connection = self.skip2_connection_residual_block(out_softmax2)
+
+        out_mpool3 = self.mpool3(out_softmax2)
+        out_softmax3 = self.softmax3_blocks(out_mpool3)
+        out_interp3 = self.interpolation3(out_softmax3) + out_softmax2
+        out = out_interp3 + out_skip2_connection
+
+        out_softmax4 = self.softmax4_blocks(out)
+        out_interp2 = self.interpolation2(out_softmax4) + out_softmax1
+        out = out_interp2 + out_skip1_connection
+
+        out_softmax5 = self.softmax5_blocks(out)
+        out_interp1 = self.interpolation1(out_softmax5) + out_trunk
+
+        out_softmax6 = self.softmax6_blocks(out_interp1)
+        out = (1 + out_softmax6) * out_trunk
+
+        out_last = self.last_blocks(out)
+        return out_last
+
+
+class AttentionModule_stage2(nn.Module):
+    # input image size is 28*28
+    def __init__(self, in_channels, out_channels, size1=(28, 28), size2=(14, 14)):
+        super(AttentionModule_stage2, self).__init__()
+        self.first_residual_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.trunk_branches = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+         )
+
+        self.mpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.softmax1_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.skip1_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool2 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.softmax2_blocks = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+        )
+
+        self.interpolation2 = nn.UpsamplingBilinear2d(size=size2)
+
+        self.softmax3_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.interpolation1 = nn.UpsamplingBilinear2d(size=size1)
+
+        self.softmax4_blocks = nn.Sequential(
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.Sigmoid()
+        )
+
+        self.last_blocks = ResidualBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        x = self.first_residual_blocks(x)
+        out_trunk = self.trunk_branches(x)
+        out_mpool1 = self.mpool1(x)
+        out_softmax1 = self.softmax1_blocks(out_mpool1)
+        out_skip1_connection = self.skip1_connection_residual_block(out_softmax1)
+        out_mpool2 = self.mpool2(out_softmax1)
+        out_softmax2 = self.softmax2_blocks(out_mpool2)
+
+        out_interp2 = self.interpolation2(out_softmax2) + out_softmax1
+        # print(out_skip2_connection.data)
+        # print(out_interp3.data)
+        out = out_interp2 + out_skip1_connection
+        out_softmax3 = self.softmax3_blocks(out)
+        out_interp1 = self.interpolation1(out_softmax3) + out_trunk
+        out_softmax4 = self.softmax4_blocks(out_interp1)
+        out = (1 + out_softmax4) * out_trunk
+        out_last = self.last_blocks(out)
+
+        return out_last
+
+
+class AttentionModule_stage3(nn.Module):
+    # input image size is 14*14
+    def __init__(self, in_channels, out_channels, size1=(14, 14)):
+        super(AttentionModule_stage3, self).__init__()
+        self.first_residual_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.trunk_branches = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+         )
+
+        self.mpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.softmax1_blocks = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+        )
+
+        self.interpolation1 = nn.UpsamplingBilinear2d(size=size1)
+
+        self.softmax2_blocks = nn.Sequential(
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.Sigmoid()
+        )
+
+        self.last_blocks = ResidualBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        x = self.first_residual_blocks(x)
+        out_trunk = self.trunk_branches(x)
+        out_mpool1 = self.mpool1(x)
+        out_softmax1 = self.softmax1_blocks(out_mpool1)
+
+        out_interp1 = self.interpolation1(out_softmax1) + out_trunk
+        out_softmax2 = self.softmax2_blocks(out_interp1)
+        out = (1 + out_softmax2) * out_trunk
+        out_last = self.last_blocks(out)
+
+        return out_last
+
+
+class ResidualAttentionModel_56(nn.Module):
+    # for input size 224
+    def __init__(self):
+        super(ResidualAttentionModel_56, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias = False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.mpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.residual_block1 = ResidualBlock(64, 256)
+        self.attention_module1 = AttentionModule_stage1(256, 256)
+        self.residual_block2 = ResidualBlock(256, 512, 2)
+        self.attention_module2 = AttentionModule_stage2(512, 512)
+        self.residual_block3 = ResidualBlock(512, 1024, 2)
+        self.attention_module3 = AttentionModule_stage3(1024, 1024)
+        self.residual_block4 = ResidualBlock(1024, 2048, 2)
+        self.residual_block5 = ResidualBlock(2048, 2048)
+        self.residual_block6 = ResidualBlock(2048, 2048)
+        self.mpool2 = nn.Sequential(
+            nn.BatchNorm2d(2048),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=7, stride=1)
+        )
+        self.fc = nn.Linear(2048,1000)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.mpool1(out)
+        # print(out.data)
+        out = self.residual_block1(out)
+        out = self.attention_module1(out)
+        out = self.residual_block2(out)
+        out = self.attention_module2(out)
+        out = self.residual_block3(out)
+        # print(out.data)
+        out = self.attention_module3(out)
+        out = self.residual_block4(out)
+        out = self.residual_block5(out)
+        out = self.residual_block6(out)
+        out = self.mpool2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
+
+
+class ResidualAttentionModel_92(nn.Module):
+    # for input size 224
+    def __init__(self):
+        super(ResidualAttentionModel_92, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias = False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.mpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.residual_block1 = ResidualBlock(64, 256)
+        self.attention_module1 = AttentionModule_stage1(256, 256)
+        self.residual_block2 = ResidualBlock(256, 512, 2)
+        self.attention_module2 = AttentionModule_stage2(512, 512)
+        self.attention_module2_2 = AttentionModule_stage2(512, 512)  # tbq add
+        self.residual_block3 = ResidualBlock(512, 1024, 2)
+        self.attention_module3 = AttentionModule_stage3(1024, 1024)
+        self.attention_module3_2 = AttentionModule_stage3(1024, 1024)  # tbq add
+        self.attention_module3_3 = AttentionModule_stage3(1024, 1024)  # tbq add
+        self.residual_block4 = ResidualBlock(1024, 2048, 2)
+        self.residual_block5 = ResidualBlock(2048, 2048)
+        self.residual_block6 = ResidualBlock(2048, 2048)
+        self.mpool2 = nn.Sequential(
+            nn.BatchNorm2d(2048),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=7, stride=1)
+        )
+        self.fc = nn.Linear(2048,1000)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.mpool1(out)
+        # print(out.data)
+        out = self.residual_block1(out)
+        out = self.attention_module1(out)
+        out = self.residual_block2(out)
+        out = self.attention_module2(out)
+        out = self.attention_module2_2(out)
+        out = self.residual_block3(out)
+        # print(out.data)
+        out = self.attention_module3(out)
+        out = self.attention_module3_2(out)
+        out = self.attention_module3_3(out)
+        out = self.residual_block4(out)
+        out = self.residual_block5(out)
+        out = self.residual_block6(out)
+        out = self.mpool2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
+
+
+def oth_resattnet56(pretrained=False, **kwargs):
+    return ResidualAttentionModel_56(**kwargs)
+
+
+def oth_resattnet92(pretrained=False, **kwargs):
+    return ResidualAttentionModel_92(**kwargs)
+
+
+def _calc_width(net):
+    import numpy as np
+    net_params = filter(lambda p: p.requires_grad, net.parameters())
+    weight_count = 0
+    for param in net_params:
+        weight_count += np.prod(param.size())
+    return weight_count
+
+
+def _test():
+    import torch
+    from torch.autograd import Variable
+
+    pretrained = False
+
+    models = [
+        oth_resattnet56,
+        oth_resattnet92,
+    ]
+
+    for model in models:
+
+        net = model(pretrained=pretrained)
+
+        # net.train()
+        net.eval()
+        weight_count = _calc_width(net)
+        print("m={}, {}".format(model.__name__, weight_count))
+        assert (model != oth_resattnet56 or weight_count == 49636520)
+        assert (model != oth_resattnet92 or weight_count == 85234344)
+
+        x = Variable(torch.randn(1, 3, 224, 224))
+        y = net(x)
+        assert (tuple(y.size()) == (1, 1000))
+
+
+if __name__ == "__main__":
+    _test()
+
+

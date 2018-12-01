5a7d362f8cb1bf6e1f0c22bef65719484a3ce06c
osmr <osemery@gmail.com>
2018-12-01 16:36:21 +0300

    Work on CH-CBAM-ResNet

--- chainer_/chainercv2/models/cbamresnet.py
+++ chainer_/chainercv2/models/cbamresnet.py
@@ -1,22 +1,24 @@
 """
-    CBAM-ResNet, implemented in Gluon.
+    CBAM-ResNet, implemented in Chainer.
     Original paper: 'CBAM: Convolutional Block Attention Module,' https://arxiv.org/abs/1807.06521.
 """
 
 __all__ = ['CbamResNet', 'cbam_resnet18', 'cbam_resnet34', 'cbam_resnet50', 'cbam_resnet101', 'cbam_resnet152']
 
 import os
-from mxnet import cpu
-from mxnet.gluon import nn, HybridBlock
-from .common import conv1x1_block, ConvBlock
+import chainer.functions as F
+import chainer.links as L
+from chainer import Chain
+from functools import partial
+from chainer.serializers import load_npz
+from .common import SimpleSequential, conv1x1_block, ConvBlock
 from .resnet import ResInitBlock, ResBlock, ResBottleneck
 
 
 def conv7x7_block(in_channels,
                   out_channels,
-                  strides=1,
-                  padding=3,
-                  bn_use_global_stats=False,
+                  stride=1,
+                  pad=3,
                   activate=True):
     """
     7x7 version of the standard convolution block.
@@ -27,27 +29,24 @@ def conv7x7_block(in_channels,
         Number of input channels.
     out_channels : int
         Number of output channels.
-    strides : int or tuple/list of 2 int, default 1
-        Strides of the convolution.
-    padding : int or tuple/list of 2 int, default 3
+    stride : int or tuple/list of 2 int, default 1
+        Stride of the convolution.
+    pad : int or tuple/list of 2 int, default 3
         Padding value for convolution layer.
-    bn_use_global_stats : bool, default False
-        Whether global moving statistics is used instead of local batch-norm for BatchNorm layers.
     activate : bool, default True
         Whether activate the convolution block.
     """
     return ConvBlock(
         in_channels=in_channels,
         out_channels=out_channels,
-        kernel_size=7,
-        strides=strides,
-        padding=padding,
+        ksize=7,
+        stride=stride,
+        pad=pad,
         use_bias=False,
-        bn_use_global_stats=bn_use_global_stats,
         activate=activate)
 
 
-class MLP(HybridBlock):
+class MLP(Chain):
     """
     Multilayer perceptron block.
 
@@ -60,30 +59,28 @@ class MLP(HybridBlock):
     """
     def __init__(self,
                  channels,
-                 reduction_ratio=16,
-                 **kwargs):
-        super(MLP, self).__init__(**kwargs)
+                 reduction_ratio=16):
+        super(MLP, self).__init__()
         mid_channels = channels // reduction_ratio
 
-        with self.name_scope():
-            self.flatten = nn.Flatten()
-            self.fc1 = nn.Dense(
-                units=mid_channels,
-                in_units=channels)
-            self.activ = nn.Activation('relu')
-            self.fc2 = nn.Dense(
-                units=channels,
-                in_units=mid_channels)
+        with self.init_scope():
+            self.fc1 = L.Linear(
+                in_size=channels,
+                out_size=mid_channels)
+            self.activ = F.relu
+            self.fc2 = L.Linear(
+                in_size=mid_channels,
+                out_size=channels)
 
-    def hybrid_forward(self, F, x):
-        x = self.flatten(x)
+    def __call__(self, x):
+        x = F.reshape(x, shape=(x.shape[0], -1))
         x = self.fc1(x)
         x = self.activ(x)
         x = self.fc2(x)
         return x
 
 
-class ChannelGate(HybridBlock):
+class ChannelGate(Chain):
     """
     CBAM channel gate block.
 
@@ -96,61 +93,48 @@ class ChannelGate(HybridBlock):
     """
     def __init__(self,
                  channels,
-                 reduction_ratio=16,
-                 **kwargs):
-        super(ChannelGate, self).__init__(**kwargs)
-        with self.name_scope():
-            self.avg_pool = nn.GlobalAvgPool2D()
-            self.max_pool = nn.GlobalMaxPool2D()
+                 reduction_ratio=16):
+        super(ChannelGate, self).__init__()
+        with self.init_scope():
             self.mlp = MLP(
                 channels=channels,
                 reduction_ratio=reduction_ratio)
-            self.sigmoid = nn.Activation('sigmoid')
 
-    def hybrid_forward(self, F, x):
-        att1 = self.avg_pool(x)
+    def __call__(self, x):
+        att1 = F.average_pooling_2d(x, ksize=x.shape[2:])
         att1 = self.mlp(att1)
-        att2 = self.max_pool(x)
+        att2 = F.max_pooling_2d(x, ksize=x.shape[2:])
         att2 = self.mlp(att2)
         att = att1 + att2
-        att = self.sigmoid(att)
-        att = att.expand_dims(2).expand_dims(3).broadcast_like(x)
+        att = F.sigmoid(att)
+        att = F.broadcast_to(F.expand_dims(F.expand_dims(att, axis=2), axis=3), x.shape)
         x = x * att
         return x
 
 
-class SpatialGate(HybridBlock):
+class SpatialGate(Chain):
     """
     CBAM spatial gate block.
-
-    Parameters:
-    ----------
-    bn_use_global_stats : bool
-        Whether global moving statistics is used instead of local batch-norm for BatchNorm layers.
     """
-    def __init__(self,
-                 bn_use_global_stats,
-                 **kwargs):
-        super(SpatialGate, self).__init__(**kwargs)
-        with self.name_scope():
+    def __init__(self):
+        super(SpatialGate, self).__init__()
+        with self.init_scope():
             self.conv = conv7x7_block(
                 in_channels=2,
                 out_channels=1,
-                bn_use_global_stats=bn_use_global_stats,
                 activate=False)
-            self.sigmoid = nn.Activation('sigmoid')
 
-    def hybrid_forward(self, F, x):
-        att1 = x.max(axis=1).expand_dims(1)
-        att2 = x.mean(axis=1).expand_dims(1)
-        att = F.concat(att1, att2, dim=1)
+    def __call__(self, x):
+        att1 = F.expand_dims(F.max(x, axis=1), axis=1)
+        att2 = F.expand_dims(F.mean(x, axis=1), axis=1)
+        att = F.concat((att1, att2), axis=1)
         att = self.conv(att)
-        att = self.sigmoid(att).broadcast_like(x)
+        att = F.broadcast_to(F.sigmoid(att), x.shape)
         x = x * att
         return x
 
 
-class CbamBlock(HybridBlock):
+class CbamBlock(Chain):
     """
     CBAM attention block for CBAM-ResNet.
 
@@ -160,28 +144,24 @@ class CbamBlock(HybridBlock):
         Number of input/output channels.
     reduction_ratio : int, default 16
         Channel reduction ratio.
-    bn_use_global_stats : bool, default False
-        Whether global moving statistics is used instead of local batch-norm for BatchNorm layers.
     """
     def __init__(self,
                  channels,
-                 reduction_ratio=16,
-                 bn_use_global_stats=False,
-                 **kwargs):
-        super(CbamBlock, self).__init__(**kwargs)
-        with self.name_scope():
+                 reduction_ratio=16):
+        super(CbamBlock, self).__init__()
+        with self.init_scope():
             self.ch_gate = ChannelGate(
                 channels=channels,
                 reduction_ratio=reduction_ratio)
-            self.sp_gate = SpatialGate(bn_use_global_stats=bn_use_global_stats)
+            self.sp_gate = SpatialGate()
 
-    def hybrid_forward(self, F, x):
+    def __call__(self, x):
         x = self.ch_gate(x)
         x = self.sp_gate(x)
         return x
 
 
-class CbamResUnit(HybridBlock):
+class CbamResUnit(Chain):
     """
     CBAM-ResNet unit.
 
@@ -191,48 +171,41 @@ class CbamResUnit(HybridBlock):
         Number of input channels.
     out_channels : int
         Number of output channels.
-    strides : int or tuple/list of 2 int
-        Strides of the convolution.
-    bn_use_global_stats : bool
-        Whether global moving statistics is used instead of local batch-norm for BatchNorm layers.
+    stride : int or tuple/list of 2 int
+        Stride of the convolution.
     bottleneck : bool
         Whether to use a bottleneck or simple block in units.
     """
     def __init__(self,
                  in_channels,
                  out_channels,
-                 strides,
-                 bn_use_global_stats,
-                 bottleneck,
-                 **kwargs):
-        super(CbamResUnit, self).__init__(**kwargs)
-        self.resize_identity = (in_channels != out_channels) or (strides != 1)
+                 stride,
+                 bottleneck):
+        super(CbamResUnit, self).__init__()
+        self.resize_identity = (in_channels != out_channels) or (stride != 1)
 
-        with self.name_scope():
+        with self.init_scope():
             if bottleneck:
                 self.body = ResBottleneck(
                     in_channels=in_channels,
                     out_channels=out_channels,
-                    strides=strides,
-                    bn_use_global_stats=bn_use_global_stats,
+                    stride=stride,
                     conv1_stride=False)
             else:
                 self.body = ResBlock(
                     in_channels=in_channels,
                     out_channels=out_channels,
-                    strides=strides,
-                    bn_use_global_stats=bn_use_global_stats)
+                    stride=stride)
             if self.resize_identity:
                 self.identity_conv = conv1x1_block(
                     in_channels=in_channels,
                     out_channels=out_channels,
-                    strides=strides,
-                    bn_use_global_stats=bn_use_global_stats,
+                    stride=stride,
                     activate=False)
             self.cbam = CbamBlock(channels=out_channels)
-            self.activ = nn.Activation('relu')
+            self.activ = F.relu
 
-    def hybrid_forward(self, F, x):
+    def __call__(self, x):
         if self.resize_identity:
             identity = self.identity_conv(x)
         else:
@@ -244,7 +217,7 @@ class CbamResUnit(HybridBlock):
         return x
 
 
-class CbamResNet(HybridBlock):
+class CbamResNet(Chain):
     """
     CBAM-ResNet model from 'CBAM: Convolutional Block Attention Module,' https://arxiv.org/abs/1807.06521.
 
@@ -256,9 +229,6 @@ class CbamResNet(HybridBlock):
         Number of output channels for the initial unit.
     bottleneck : bool
         Whether to use a bottleneck or simple block in units.
-    bn_use_global_stats : bool, default False
-        Whether global moving statistics is used instead of local batch-norm for BatchNorm layers.
-        Useful for fine-tuning.
     in_channels : int, default 3
         Number of input channels.
     in_size : tuple of two ints, default (224, 224)
@@ -270,46 +240,47 @@ class CbamResNet(HybridBlock):
                  channels,
                  init_block_channels,
                  bottleneck,
-                 bn_use_global_stats=False,
                  in_channels=3,
                  in_size=(224, 224),
-                 classes=1000,
-                 **kwargs):
-        super(CbamResNet, self).__init__(**kwargs)
+                 classes=1000):
+        super(CbamResNet, self).__init__()
         self.in_size = in_size
         self.classes = classes
 
-        with self.name_scope():
-            self.features = nn.HybridSequential(prefix='')
-            self.features.add(ResInitBlock(
-                in_channels=in_channels,
-                out_channels=init_block_channels,
-                bn_use_global_stats=bn_use_global_stats))
-            in_channels = init_block_channels
-            for i, channels_per_stage in enumerate(channels):
-                stage = nn.HybridSequential(prefix='stage{}_'.format(i + 1))
-                with stage.name_scope():
-                    for j, out_channels in enumerate(channels_per_stage):
-                        strides = 2 if (j == 0) and (i != 0) else 1
-                        stage.add(CbamResUnit(
-                            in_channels=in_channels,
-                            out_channels=out_channels,
-                            strides=strides,
-                            bn_use_global_stats=bn_use_global_stats,
-                            bottleneck=bottleneck))
-                        in_channels = out_channels
-                self.features.add(stage)
-            self.features.add(nn.AvgPool2D(
-                pool_size=7,
-                strides=1))
+        with self.init_scope():
+            self.features = SimpleSequential()
+            with self.features.init_scope():
+                setattr(self.features, "init_block", ResInitBlock(
+                    in_channels=in_channels,
+                    out_channels=init_block_channels))
+                in_channels = init_block_channels
+                for i, channels_per_stage in enumerate(channels):
+                    stage = SimpleSequential()
+                    with stage.init_scope():
+                        for j, out_channels in enumerate(channels_per_stage):
+                            stride = 2 if (j == 0) and (i != 0) else 1
+                            setattr(stage, "unit{}".format(j + 1), CbamResUnit(
+                                in_channels=in_channels,
+                                out_channels=out_channels,
+                                stride=stride,
+                                bottleneck=bottleneck))
+                            in_channels = out_channels
+                    setattr(self.features, "stage{}".format(i + 1), stage)
+                setattr(self.features, 'final_pool', partial(
+                    F.average_pooling_2d,
+                    ksize=7,
+                    stride=1))
 
-            self.output = nn.HybridSequential(prefix='')
-            self.output.add(nn.Flatten())
-            self.output.add(nn.Dense(
-                units=classes,
-                in_units=in_channels))
+            self.output = SimpleSequential()
+            with self.output.init_scope():
+                setattr(self.output, 'flatten', partial(
+                    F.reshape,
+                    shape=(-1, in_channels)))
+                setattr(self.output, 'fc', L.Linear(
+                    in_size=in_channels,
+                    out_size=classes))
 
-    def hybrid_forward(self, F, x):
+    def __call__(self, x):
         x = self.features(x)
         x = self.output(x)
         return x
@@ -318,8 +289,7 @@ class CbamResNet(HybridBlock):
 def get_resnet(blocks,
                model_name=None,
                pretrained=False,
-               ctx=cpu(),
-               root=os.path.join('~', '.mxnet', 'models'),
+               root=os.path.join('~', '.chainer', 'models'),
                **kwargs):
     """
     Create CBAM-ResNet model with specific parameters.
@@ -338,9 +308,7 @@ def get_resnet(blocks,
         Model name for loading pretrained model.
     pretrained : bool, default False
         Whether to load the pretrained weights for model.
-    ctx : Context, default CPU
-        The context in which to load the pretrained weights.
-    root : str, default '~/.mxnet/models'
+    root : str, default '~/.chainer/models'
         Location for keeping the model parameters.
     """
 
@@ -378,11 +346,11 @@ def get_resnet(blocks,
         if (model_name is None) or (not model_name):
             raise ValueError("Parameter `model_name` should be properly initialized for loading pretrained model.")
         from .model_store import get_model_file
-        net.load_parameters(
-            filename=get_model_file(
+        load_npz(
+            file=get_model_file(
                 model_name=model_name,
                 local_model_store_dir_path=root),
-            ctx=ctx)
+            obj=net)
 
     return net
 
@@ -395,9 +363,7 @@ def cbam_resnet18(**kwargs):
     ----------
     pretrained : bool, default False
         Whether to load the pretrained weights for model.
-    ctx : Context, default CPU
-        The context in which to load the pretrained weights.
-    root : str, default '~/.mxnet/models'
+    root : str, default '~/.chainer/models'
         Location for keeping the model parameters.
     """
     return get_resnet(blocks=18, model_name="cbam_resnet18", **kwargs)
@@ -411,9 +377,7 @@ def cbam_resnet34(**kwargs):
     ----------
     pretrained : bool, default False
         Whether to load the pretrained weights for model.
-    ctx : Context, default CPU
-        The context in which to load the pretrained weights.
-    root : str, default '~/.mxnet/models'
+    root : str, default '~/.chainer/models'
         Location for keeping the model parameters.
     """
     return get_resnet(blocks=34, model_name="cbam_resnet34", **kwargs)
@@ -427,9 +391,7 @@ def cbam_resnet50(**kwargs):
     ----------
     pretrained : bool, default False
         Whether to load the pretrained weights for model.
-    ctx : Context, default CPU
-        The context in which to load the pretrained weights.
-    root : str, default '~/.mxnet/models'
+    root : str, default '~/.chainer/models'
         Location for keeping the model parameters.
     """
     return get_resnet(blocks=50, model_name="cbam_resnet50", **kwargs)
@@ -443,9 +405,7 @@ def cbam_resnet101(**kwargs):
     ----------
     pretrained : bool, default False
         Whether to load the pretrained weights for model.
-    ctx : Context, default CPU
-        The context in which to load the pretrained weights.
-    root : str, default '~/.mxnet/models'
+    root : str, default '~/.chainer/models'
         Location for keeping the model parameters.
     """
     return get_resnet(blocks=101, model_name="cbam_resnet101", **kwargs)
@@ -459,9 +419,7 @@ def cbam_resnet152(**kwargs):
     ----------
     pretrained : bool, default False
         Whether to load the pretrained weights for model.
-    ctx : Context, default CPU
-        The context in which to load the pretrained weights.
-    root : str, default '~/.mxnet/models'
+    root : str, default '~/.chainer/models'
         Location for keeping the model parameters.
     """
     return get_resnet(blocks=152, model_name="cbam_resnet152", **kwargs)
@@ -469,33 +427,24 @@ def cbam_resnet152(**kwargs):
 
 def _test():
     import numpy as np
-    import mxnet as mx
+    import chainer
+
+    chainer.global_config.train = False
 
     pretrained = False
 
     models = [
-        # cbam_resnet18,
-        # cbam_resnet34,
+        cbam_resnet18,
+        cbam_resnet34,
         cbam_resnet50,
-        # cbam_resnet101,
-        # cbam_resnet152,
+        cbam_resnet101,
+        cbam_resnet152,
     ]
 
     for model in models:
 
         net = model(pretrained=pretrained)
-
-        ctx = mx.cpu()
-        if not pretrained:
-            net.initialize(ctx=ctx)
-
-        net.hybridize()
-        net_params = net.collect_params()
-        weight_count = 0
-        for param in net_params.values():
-            if (param.shape is None) or (not param._differentiable):
-                continue
-            weight_count += np.prod(param.shape)
+        weight_count = net.count_params()
         print("m={}, {}".format(model.__name__, weight_count))
         assert (model != cbam_resnet18 or weight_count == 11779392)
         assert (model != cbam_resnet34 or weight_count == 21960468)
@@ -503,9 +452,9 @@ def _test():
         assert (model != cbam_resnet101 or weight_count == 49330172)
         assert (model != cbam_resnet152 or weight_count == 66826848)
 
-        x = mx.nd.zeros((2, 3, 224, 224), ctx=ctx)
+        x = np.zeros((1, 3, 224, 224), np.float32)
         y = net(x)
-        assert (y.shape == (2, 1000))
+        assert (y.shape == (1, 1000))
 
 
 if __name__ == "__main__":

43128230cf52eae0e4b3a38c0cc73d68790c4c9e
osmr <osemery@gmail.com>
2019-02-06 22:37:47 +0300

    Update PIP-packages

--- pytorch/setup.py
+++ pytorch/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pytorchcv',
-    version='0.0.34',
+    version='0.0.35',
     description='Image classification models for PyTorch',
     license='MIT',
     long_description=long_description,

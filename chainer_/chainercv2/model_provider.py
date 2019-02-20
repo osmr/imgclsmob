da051e90eb54922a7488c680428da1189397d3d7
osmr <osemery@gmail.com>
2019-02-20 23:51:37 +0300

    Working on ESPNetv2, 8

--- chainer_/chainercv2/model_provider.py
+++ chainer_/chainercv2/model_provider.py
@@ -63,6 +63,8 @@ from .models.xdensenet import *
 from .models.xdensenet_cifar import *
 from .models.ror_cifar import *
 
+from .models.espnetv2 import *
+
 __all__ = ['get_model']
 
 
@@ -434,6 +436,12 @@ _models = {
     'ror3_110_cifar100': ror3_110_cifar100,
     'ror3_164_cifar10': ror3_164_cifar10,
     'ror3_164_cifar100': ror3_164_cifar100,
+
+    'espnetv2_wd2': espnetv2_wd2,
+    'espnetv2_w1': espnetv2_w1,
+    'espnetv2_w5d8': espnetv2_w5d8,
+    'espnetv2_w3d2': espnetv2_w3d2,
+    'espnetv2_w2': espnetv2_w2,
 }
 
 

0168d5298ab0878818e44b311678ff683f5ee0e2
osmr <osemery@gmail.com>
2019-03-05 09:47:03 +0300

    Working on iRevNet, 2

--- pytorch/pytorchcv/models/others/oth_irevnet2.py
+++ pytorch/pytorchcv/models/others/oth_irevnet2.py
@@ -19,7 +19,7 @@ def split(x):
 
 
 def merge(x1, x2):
-    return torch.cat((x1, x2), 1)
+    return torch.cat((x1, x2), dim=1)
 
 
 class injective_pad(nn.Module):
@@ -38,7 +38,8 @@ class injective_pad(nn.Module):
 
 
 class psi(nn.Module):
-    def __init__(self, block_size):
+    def __init__(self,
+                 block_size):
         super(psi, self).__init__()
         self.block_size = block_size
         self.block_size_sq = block_size*block_size
@@ -70,11 +71,15 @@ class psi(nn.Module):
 
 
 class irevnet_block(nn.Module):
-    def __init__(self, in_ch, out_ch, stride=1, first=False, dropout_rate=0.,
-                 affineBN=True, mult=4):
+    def __init__(self,
+                 in_ch,
+                 out_ch,
+                 stride=1,
+                 first=False,
+                 dropout_rate=0.,
+                 mult=4):
         """ buid invertible bottleneck block """
         super(irevnet_block, self).__init__()
-        self.first = first
         self.pad = 2 * out_ch - in_ch
         self.stride = stride
         self.inj_pad = injective_pad(self.pad)
@@ -86,19 +91,32 @@ class irevnet_block(nn.Module):
             print('')
         layers = []
         if not first:
-            layers.append(nn.BatchNorm2d(in_ch//2, affine=affineBN))
+            layers.append(nn.BatchNorm2d(in_ch//2))
             layers.append(nn.ReLU(inplace=True))
-        layers.append(nn.Conv2d(in_ch//2, int(out_ch//mult), kernel_size=3,
-                      stride=stride, padding=1, bias=False))
-        layers.append(nn.BatchNorm2d(int(out_ch//mult), affine=affineBN))
+        layers.append(nn.Conv2d(
+            in_ch//2,
+            int(out_ch//mult),
+            kernel_size=3,
+            stride=stride,
+            padding=1,
+            bias=False))
+        layers.append(nn.BatchNorm2d(int(out_ch//mult)))
         layers.append(nn.ReLU(inplace=True))
-        layers.append(nn.Conv2d(int(out_ch//mult), int(out_ch//mult),
-                      kernel_size=3, padding=1, bias=False))
+        layers.append(nn.Conv2d(
+            int(out_ch//mult),
+            int(out_ch//mult),
+            kernel_size=3,
+            padding=1,
+            bias=False))
         layers.append(nn.Dropout(p=dropout_rate))
-        layers.append(nn.BatchNorm2d(int(out_ch//mult), affine=affineBN))
+        layers.append(nn.BatchNorm2d(int(out_ch//mult)))
         layers.append(nn.ReLU(inplace=True))
-        layers.append(nn.Conv2d(int(out_ch//mult), out_ch, kernel_size=3,
-                      padding=1, bias=False))
+        layers.append(nn.Conv2d(
+            int(out_ch//mult),
+            out_ch,
+            kernel_size=3,
+            padding=1,
+            bias=False))
         self.bottleneck_block = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -138,91 +156,71 @@ class irevnet_block(nn.Module):
 
 class iRevNet(nn.Module):
     def __init__(self,
-                 nBlocks,
-                 nStrides,
-                 nChannels,
-                 init_ds=2,
-                 dropout_rate=0.,
-                 affineBN=True,
+                 channels,
+                 init_block_channels,
                  mult=4,
+                 dropout_rate=0.0,
                  in_channels=3,
                  in_size=(224, 224),
                  num_classes=1000):
         super(iRevNet, self).__init__()
+        assert (in_channels > 0)
+        self.in_size = in_size
+        self.num_classes = num_classes
 
-        self.ds = in_size[1] // 2**(nStrides.count(2)+init_ds//2)
-        self.init_ds = init_ds
-        self.in_ch = in_channels * 2**self.init_ds
-        self.first = True
+        self.init_psi = psi(block_size=2)
+        in_channels = init_block_channels
 
-        self.init_psi = psi(self.init_ds)
-        self.stack = self.irevnet_stack(
-            irevnet_block,
-            nChannels,
-            nBlocks,
-            nStrides,
-            dropout_rate=dropout_rate,
-            affineBN=affineBN,
-            in_ch=self.in_ch,
-            mult=mult)
-        self.bn1 = nn.BatchNorm2d(nChannels[-1]*2, momentum=0.9)
-        self.linear = nn.Linear(nChannels[-1] * 2, num_classes)
-
-    def irevnet_stack(self,
-                      _block,
-                      nChannels,
-                      nBlocks,
-                      nStrides,
-                      dropout_rate,
-                      affineBN,
-                      in_ch,
-                      mult):
-        """ Create stack of irevnet blocks """
         block_list = nn.ModuleList()
-        strides = []
-        channels = []
-        for channel, depth, stride in zip(nChannels, nBlocks, nStrides):
-            strides = strides + ([stride] + [1]*(depth-1))
-            channels = channels + ([channel]*depth)
-        for channel, stride in zip(channels, strides):
-            block_list.append(_block(
-                in_ch,
-                channel,
-                stride,
-                first=self.first,
-                dropout_rate=dropout_rate,
-                affineBN=affineBN,
-                mult=mult))
-            in_ch = 2 * channel
-            self.first = False
-        return block_list
+        first = True
+        for i, channels_per_stage in enumerate(channels):
+            for j, out_channels in enumerate(channels_per_stage):
+                stride = 2 if (j == 0) else 1
+                block_list.append(irevnet_block(
+                    in_channels,
+                    out_channels,
+                    stride,
+                    first=first,
+                    dropout_rate=dropout_rate,
+                    mult=mult))
+                in_channels = 2 * out_channels
+                first = False
+        self.stack = block_list
+
+        self.bn1 = nn.BatchNorm2d(in_channels, momentum=0.9)
+
+        self.features = nn.Sequential()
+        self.features.add_module("final_pool", nn.AvgPool2d(
+            kernel_size=7,
+            stride=1))
+        self.output = nn.Linear(
+            in_features=in_channels,
+            out_features=num_classes)
 
     def forward(self, x):
         """ irevnet forward """
-        n = self.in_ch // 2
-        if self.init_ds != 0:
-            x = self.init_psi.forward(x)
-        out = (x[:, :n, :, :], x[:, n:, :, :])
+        x = self.init_psi.forward(x)
+
+        out = torch.chunk(x, chunks=2, dim=1)
         for block in self.stack:
             out = block.forward(out)
-        out_bij = merge(out[0], out[1])
+        out_bij = torch.cat((out[0], out[1]), dim=1)
+
         out = F.relu(self.bn1(out_bij))
-        out = F.avg_pool2d(out, self.ds)
+
+        out = self.features(out)
         out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        # return out, out_bij
+        out = self.output(out)
+
         return out
 
     def inverse(self, out_bij):
         """ irevnet inverse """
-        out = split(out_bij)
+        out = torch.chunk(out_bij, chunks=2, dim=1)
         for i in range(len(self.stack)):
             out = self.stack[-1-i].inverse(out)
-        out = merge(out[0], out[1])
-        if self.init_ds != 0:
-            x = self.init_psi.inverse(out)
-        else:
-            x = out
+        out = torch.cat((out[0], out[1]), dim=1)
+        x = self.init_psi.inverse(out)
         return x
 
 
@@ -246,23 +244,20 @@ def get_irevnet(blocks,
         Location for keeping the model parameters.
     """
     if blocks == 301:
-        nBlocks = [6, 16, 72, 6]
+        layers = [6, 16, 72, 6]
     else:
         raise ValueError("Unsupported i-RevNet with number of blocks: {}".format(blocks))
 
-    assert (sum(nBlocks) * 3 + 1 == blocks)
+    assert (sum(layers) * 3 + 1 == blocks)
 
-    nStrides = [2, 2, 2, 2]
-    nChannels = [24, 96, 384, 1536]
-    init_ds = 2
+    channels_per_layers = [24, 96, 384, 1536]
+    init_block_channels = 12
+
+    channels = [[ci] * li for (ci, li) in zip(channels_per_layers, layers)]
 
     net = iRevNet(
-        nBlocks=nBlocks,
-        nStrides=nStrides,
-        nChannels=nChannels,
-        init_ds=init_ds,
-        dropout_rate=0.,
-        affineBN=True,
+        channels=channels,
+        init_block_channels=init_block_channels,
         mult=4)
 
     return net

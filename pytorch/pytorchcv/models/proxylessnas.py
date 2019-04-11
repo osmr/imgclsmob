6c43ab833292df987298a0155dec6c9a894ef7ae
osmr <osemery@gmail.com>
2019-04-11 22:33:00 +0300

    Woking on Proxyless, 4

--- pytorch/pytorchcv/models/proxylessnas.py
+++ pytorch/pytorchcv/models/proxylessnas.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from common import ConvBlock
+from .common import ConvBlock, conv1x1_block, conv3x3_block
 
 
 def get_same_padding(kernel_size):
@@ -13,115 +13,15 @@ def get_same_padding(kernel_size):
     return kernel_size // 2
 
 
-def set_layer_from_config(layer_config):
-    if layer_config is None:
-        return None
-
-    name2layer = {
-        ConvLayer.__name__: ConvLayer,
-        IdentityLayer.__name__: IdentityLayer,
-        MBInvertedConvLayer.__name__: MBInvertedConvLayer,
-        ZeroLayer.__name__: ZeroLayer,
-    }
-
-    layer_name = layer_config.pop('name')
-    layer = name2layer[layer_name]
-    return layer.build_from_config(layer_config)
-
-
-class BasicUnit(nn.Module):
-
-    def forward(self, x):
-        raise NotImplementedError
-
-    @staticmethod
-    def build_from_config(config):
-        raise NotImplementedError
-
-
-class IdentityLayer(BasicUnit):
-
-    def __init__(self,
-                 **kwargs):
-        super(IdentityLayer, self).__init__()
-
-    def forward(self, x):
-        return x
-
-    @staticmethod
-    def build_from_config(config):
-        return IdentityLayer(**config)
-
-    @staticmethod
-    def is_zero_layer():
-        return False
-
-
-class ZeroLayer(BasicUnit):
-
-    def __init__(self, stride):
-        super(ZeroLayer, self).__init__()
-        self.stride = stride
-
-    def forward(self, x):
-        return None
-
-    @staticmethod
-    def build_from_config(config):
-        return ZeroLayer(**config)
-
-    @staticmethod
-    def is_zero_layer():
-        return True
-
-
-class ConvLayer(BasicUnit):
+class MBInvertedConvLayer(nn.Module):
 
     def __init__(self,
                  in_channels,
                  out_channels,
                  kernel_size=3,
                  stride=1,
+                 expand_ratio=6,
                  **kwargs):
-        super(ConvLayer, self).__init__()
-
-        padding = get_same_padding(kernel_size)
-        self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            dilation=1,
-            groups=1,
-            bias=False)
-
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.activation = nn.ReLU6(inplace=True)
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.activation(x)
-        return x
-
-    @staticmethod
-    def build_from_config(config):
-        return ConvLayer(**config)
-
-    @staticmethod
-    def is_zero_layer():
-        return False
-
-
-class MBInvertedConvLayer(BasicUnit):
-
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 kernel_size=3,
-                 stride=1,
-                 expand_ratio=6):
         super(MBInvertedConvLayer, self).__init__()
         assert (expand_ratio >= 1)
         mid_channels = round(in_channels * expand_ratio)
@@ -163,31 +63,14 @@ class MBInvertedConvLayer(BasicUnit):
         x = self.point_linear(x)
         return x
 
-    @staticmethod
-    def build_from_config(config):
-        return MBInvertedConvLayer(**config)
 
-    @staticmethod
-    def is_zero_layer():
-        return False
-
-
-class MobileInvertedResidualBlock(BasicUnit):
+class ProxylessUnit(nn.Module):
     def __init__(self,
                  mobile_inverted_conv,
-                 shortcut):
-        super(MobileInvertedResidualBlock, self).__init__()
-
-        assert (type(mobile_inverted_conv) in [MBInvertedConvLayer, ZeroLayer])
-        assert (shortcut is None) or (type(shortcut) == IdentityLayer)
-
-        if type(mobile_inverted_conv) == ZeroLayer:
-            self.mobile_inverted_conv = None
-        else:
-            self.mobile_inverted_conv = mobile_inverted_conv
-
-        # self.shortcut = shortcut
-        self.use_shortcut = (shortcut is not None)
+                 use_shortcut):
+        super(ProxylessUnit, self).__init__()
+        self.mobile_inverted_conv = mobile_inverted_conv
+        self.use_shortcut = use_shortcut
 
     def forward(self, x):
         if self.mobile_inverted_conv is None:
@@ -197,93 +80,134 @@ class MobileInvertedResidualBlock(BasicUnit):
             res = self.mobile_inverted_conv(x)
         else:
             assert (type(self.mobile_inverted_conv) == MBInvertedConvLayer)
-            # assert (type(self.shortcut) == IdentityLayer)
             conv_x = self.mobile_inverted_conv(x)
-            # skip_x = self.shortcut(x)
             skip_x = x
             res = skip_x + conv_x
         return res
 
-    @staticmethod
-    def build_from_config(config):
-        mobile_inverted_conv = set_layer_from_config(config['mobile_inverted_conv'])
-        shortcut = set_layer_from_config(config['shortcut'])
-        return MobileInvertedResidualBlock(mobile_inverted_conv, shortcut)
 
-
-class ProxylessNASNets(BasicUnit):
+class ProxylessNAS(nn.Module):
 
     def __init__(self,
-                 first_conv,
-                 blocks,
-                 feature_mix_layer,
-                 classifier_in_features,
+                 blocks_config,
+                 channels,
+                 init_block_channels,
+                 final_block_channels,
+                 shortcut_list,
+                 in_channels=3,
                  num_classes=1000):
-        super(ProxylessNASNets, self).__init__()
+        super(ProxylessNAS, self).__init__()
 
-        self.first_conv = first_conv
-        self.blocks = nn.ModuleList(blocks)
-        self.feature_mix_layer = feature_mix_layer
-        self.global_avg_pooling = nn.AdaptiveAvgPool2d(1)
+        self.features = nn.Sequential()
+        self.features.add_module("init_block", conv3x3_block(
+            in_channels=in_channels,
+            out_channels=init_block_channels,
+            stride=2))
+        in_channels = init_block_channels
 
-        self.classifier = nn.Linear(
-            in_features=classifier_in_features,
-            out_features=num_classes,
-            bias=True)
+        for i, block_config in enumerate(blocks_config):
+            in_channels = -1
+            out_channels = -1
+            kernel_size = -1
+            stride = -1
+            expand_ratio = -1
+
+            mobile_inverted_conv_config = block_config['mobile_inverted_conv']
+            mobile_inverted_conv_layer_name = mobile_inverted_conv_config['name']
+            if mobile_inverted_conv_layer_name == "ZeroLayer":
+                mobile_inverted_conv = None
+            else:
+                in_channels = mobile_inverted_conv_config['in_channels']
+                out_channels = mobile_inverted_conv_config['out_channels']
+                kernel_size = mobile_inverted_conv_config['kernel_size']
+                stride = mobile_inverted_conv_config['stride']
+                expand_ratio = mobile_inverted_conv_config['expand_ratio']
+                mobile_inverted_conv = MBInvertedConvLayer(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=kernel_size,
+                    stride=stride,
+                    expand_ratio=expand_ratio)
+
+            print("i={}, in_channels={}, out_channels={}, stride={}".format(i, in_channels, out_channels, stride))
+
+            use_shortcut = (shortcut_list[i] == 1)
+
+            self.features.add_module("unit{}".format(i + 1), ProxylessUnit(
+                mobile_inverted_conv=mobile_inverted_conv,
+                use_shortcut=use_shortcut))
+
+        self.features.add_module("final_block", conv1x1_block(
+            in_channels=channels[-1],
+            out_channels=final_block_channels))
+        in_channels = final_block_channels
+        self.features.add_module("final_pool", nn.AvgPool2d(
+            kernel_size=7,
+            stride=1))
+
+        self.output = nn.Linear(
+            in_features=in_channels,
+            out_features=num_classes)
 
     def forward(self, x):
-        x = self.first_conv(x)
-        for block in self.blocks:
-            x = block(x)
-        if self.feature_mix_layer:
-            x = self.feature_mix_layer(x)
-        x = self.global_avg_pooling(x)
-        x = x.view(x.size(0), -1)  # flatten
-        x = self.classifier(x)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.output(x)
         return x
 
-    @staticmethod
-    def build_from_config(config):
 
-        first_conv = set_layer_from_config(config['first_conv'])
-        feature_mix_layer = set_layer_from_config(config['feature_mix_layer'])
-        blocks = []
-        for block_config in config['blocks']:
-            blocks.append(MobileInvertedResidualBlock.build_from_config(block_config))
+def get_proxylessnas(version):
+    if version == 'cpu':
+        net_config_path = "../imgclsmob_data/proxyless/proxyless_cpu.config"
+        channels = [360]
+        init_block_channels = 40
+        final_block_channels = 1432
+    elif version == 'gpu':
+        net_config_path = "../imgclsmob_data/proxyless/proxyless_gpu.config"
+        channels = [432]
+        init_block_channels = 40
+        final_block_channels = 1728
+    elif version == 'mobile':
+        net_config_path = "../imgclsmob_data/proxyless/proxyless_mobile.config"
+        channels = [320]
+        init_block_channels = 32
+        final_block_channels = 1280
+    elif version == 'mobile14':
+        net_config_path = "../imgclsmob_data/proxyless/proxyless_mobile_14.config"
+        channels = [448]
+        init_block_channels = 48
+        final_block_channels = 1792
+    else:
+        raise ValueError("Unsupported proxylessnas version {}".format(version))
 
-        return ProxylessNASNets(
-            first_conv,
-            blocks,
-            feature_mix_layer,
-            classifier_in_features=config['classifier']['in_features'])
+    shortcut_list = [0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0]
+
+    import json
+    config = json.load(open(net_config_path, 'r'))
+    net = ProxylessNAS(
+        blocks_config=config['blocks'],
+        channels=channels,
+        init_block_channels=init_block_channels,
+        final_block_channels=final_block_channels,
+        shortcut_list=shortcut_list)
+
+    return net
 
 
 def proxylessnas_cpu(pretrained=False):
-    import json
-    net_config_path = "../imgclsmob_data/proxyless/proxyless_cpu.config"
-    net_config_json = json.load(open(net_config_path, 'r'))
-    return ProxylessNASNets.build_from_config(net_config_json)
+    return get_proxylessnas(version="cpu")
 
 
 def proxylessnas_gpu(pretrained=False):
-    import json
-    net_config_path = "../imgclsmob_data/proxyless/proxyless_gpu.config"
-    net_config_json = json.load(open(net_config_path, 'r'))
-    return ProxylessNASNets.build_from_config(net_config_json)
+    return get_proxylessnas(version="gpu")
 
 
 def proxylessnas_mobile(pretrained=False):
-    import json
-    net_config_path = "../imgclsmob_data/proxyless/proxyless_mobile.config"
-    net_config_json = json.load(open(net_config_path, 'r'))
-    return ProxylessNASNets.build_from_config(net_config_json)
+    return get_proxylessnas(version="mobile")
 
 
 def proxylessnas_mobile14(pretrained=False):
-    import json
-    net_config_path = "../imgclsmob_data/proxyless/proxyless_mobile_14.config"
-    net_config_json = json.load(open(net_config_path, 'r'))
-    return ProxylessNASNets.build_from_config(net_config_json)
+    return get_proxylessnas(version="mobile14")
 
 
 def _calc_width(net):
@@ -323,7 +247,7 @@ def _test():
 
         x = Variable(torch.randn(14, 3, 224, 224))
         y = net(x)
-        y.sum().backward()
+        # y.sum().backward()
         assert (tuple(y.size()) == (14, num_classes))
 
 

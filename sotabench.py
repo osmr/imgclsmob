5cfd753ec896c5beaab51b4c020aaad82f1fa100
osmr <osemery@gmail.com>
2019-11-04 11:45:07 +0300

    Experimenting with Sotabench, 3

--- sotabench.py
+++ sotabench.py
@@ -3,22 +3,25 @@ from pytorch.pytorchcv.model_provider import trained_model_metainfo_list
 from pytorch.pytorchcv.model_provider import get_model as ptcv_get_model
 import torchvision.transforms as transforms
 import torch
+import math
 # import os
 
-input_transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
 for model_metainfo in trained_model_metainfo_list:
+    input_image_size = model_metainfo[3]
+    resize_inv_factor = model_metainfo[4]
     ImageNet.benchmark(
         model=ptcv_get_model(model_metainfo[0], pretrained=True),
-        model_description=model_metainfo[3],
+        model_description=model_metainfo[5],
         paper_model_name=model_metainfo[1],
         paper_arxiv_id=model_metainfo[2],
-        input_transform=input_transform,
+        input_transform=transforms.Compose([
+            transforms.Resize(int(math.ceil(float(input_image_size) / resize_inv_factor))),
+            transforms.CenterCrop(input_image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225]),
+        ]),
         batch_size=200,
         num_gpu=1,
         # data_root=os.path.join("..", "imgclsmob_data", "imagenet")

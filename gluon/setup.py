72e817a9b54f0e323f1af473fe168a030bb762e1
osmr <osemery@gmail.com>
2019-12-05 21:18:45 +0300

    Update PIP-packages (gluoncv2: 0.0.53, pytorchcv: 0.0.53, chainercv2: 0.0.52, kerascv: 0.0.40, tensorflowcv: 0.0.38) (fix #43)

--- gluon/setup.py
+++ gluon/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='gluoncv2',
-    version='0.0.52',
+    version='0.0.53',
     description='Image classification and segmentation models for Gluon',
     license='MIT',
     long_description=long_description,
@@ -25,10 +25,10 @@ setup(
         'Topic :: Scientific/Engineering :: Image Recognition',
     ],
     keywords='machine-learning deep-learning neuralnetwork image-classification mxnet gluon imagenet cifar svhn vgg '
-             'resnet pyramidnet diracnet densenet condensenet wrn drn dpn darknet fishnet espnetv2 xdensnet squeezenet '
-             'squeezenext shufflenet menet mobilenet igcv3 mnasnet darts xception inception polynet nasnet pnasnet ror '
-             'proxylessnas dianet efficientnet mixnet image-segmentation voc ade20k cityscapes coco pspnet deeplabv3 '
-             'fcn',
+             'resnet pyramidnet diracnet densenet condensenet wrn drn dpn darknet fishnet espnetv2 hrnet xdensnet '
+             'squeezenet squeezenext shufflenet menet mobilenet igcv3 mnasnet darts xception inception polynet nasnet '
+             'pnasnet ror proxylessnas dianet efficientnet mixnet image-segmentation voc ade20k cityscapes coco pspnet '
+             'deeplabv3 fcn',
     packages=find_packages(exclude=['datasets', 'metrics', 'others', '*.others', 'others.*', '*.others.*']),
     include_package_data=True,
     install_requires=['numpy'],

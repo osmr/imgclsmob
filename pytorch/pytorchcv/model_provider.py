a092c0094f578d5adafb58f80543b79f46c47f0c
osmr <osemery@gmail.com>
2020-02-07 23:21:12 +0300

    Update PIP-package (pytorchcv: 0.0.57), fix #52

--- pytorch/pytorchcv/model_provider.py
+++ pytorch/pytorchcv/model_provider.py
@@ -110,7 +110,7 @@ from .models.pspnet import *
 from .models.deeplabv3 import *
 from .models.sinet import *
 
-from .models.others.oth_sinet import *
+# from .models.others.oth_sinet import *
 
 from .models.superpointnet import *
 
@@ -954,7 +954,7 @@ _models = {
     'deeplabv3_resnetd101b_cityscapes': deeplabv3_resnetd101b_cityscapes,
 
     'sinet_cityscapes': sinet_cityscapes,
-    'oth_sinet_cityscapes': oth_sinet_cityscapes,
+    # 'oth_sinet_cityscapes': oth_sinet_cityscapes,
 
     'superpointnet': superpointnet,
 }

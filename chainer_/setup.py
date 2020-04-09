63859cec4cea042bb0e93c9678e130c8b2a2c110
osmr <osemery@gmail.com>
2020-04-09 15:13:42 +0300

    Update PIP-packages (gluoncv2: 0.0.57, pytorchcv: 0.0.58, chainercv2: 0.0.56, tf2cv: 0.0.11)`

--- chainer_/setup.py
+++ chainer_/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='chainercv2',
-    version='0.0.55',
+    version='0.0.56',
     description='Image classification and segmentation models for Chainer',
     license='MIT',
     long_description=long_description,

9963f2ded56fb471cd0597a3a64e99eaeb0d64ca
osmr <osemery@gmail.com>
2021-02-11 15:56:21 +0300

    Work on LinkNet, 2

--- pytorch/pytorchcv/models/others/_linknet.py
+++ pytorch/pytorchcv/models/others/_linknet.py
@@ -8,85 +8,8 @@ __all__ = ['LinkNet', 'oth_linknet_cityscapes']
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torchvision.models import resnet
 
-
-class BasicBlock(nn.Module):
-
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, groups=1, bias=False):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=bias)
-        self.bn1 = nn.BatchNorm2d(out_planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size, 1, padding, groups=groups, bias=bias)
-        self.bn2 = nn.BatchNorm2d(out_planes)
-        self.downsample = None
-        if stride > 1:
-            self.downsample = nn.Sequential(nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
-                                            nn.BatchNorm2d(out_planes),)
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out = self.relu(out+residual)
-
-        return out
-
-
-class Encoder(nn.Module):
-
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, groups=1, bias=False):
-        super(Encoder, self).__init__()
-        self.block1 = BasicBlock(in_planes, out_planes, kernel_size, stride, padding, groups, bias)
-        self.block2 = BasicBlock(out_planes, out_planes, kernel_size, 1, padding, groups, bias)
-
-    def forward(self, x):
-        x = self.block1(x)
-        x = self.block2(x)
-
-        return x
-
-
-class Decoder(nn.Module):
-
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, output_padding=0, groups=1, bias=False):
-        # TODO bias=True
-        super(Decoder, self).__init__()
-        self.conv1 = nn.Sequential(nn.Conv2d(in_planes, in_planes//4, 1, 1, 0, bias=bias),
-                                nn.BatchNorm2d(in_planes//4),
-                                nn.ReLU(inplace=True))
-        self.tp_conv = nn.Sequential(nn.ConvTranspose2d(in_planes//4, in_planes//4, kernel_size, stride, padding, output_padding, bias=bias),
-                                nn.BatchNorm2d(in_planes//4),
-                                nn.ReLU(inplace=True))
-        self.conv2 = nn.Sequential(nn.Conv2d(in_planes//4, out_planes, 1, 1, 0, bias=bias),
-                                nn.BatchNorm2d(out_planes),
-                                nn.ReLU(inplace=True))
-
-    def forward(self, x_high_level, x_low_level):
-        x = self.conv1(x_high_level)
-        x = self.tp_conv(x)
-
-        # solution for padding issues
-        # diffY = x_low_level.size()[2] - x_high_level.size()[2]
-        # diffX = x_low_level.size()[3] - x_high_level.size()[3]
-        # x = F.pad(x, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
-
-        x = center_crop(x, x_low_level.size()[2], x_low_level.size()[3])
-
-        x = self.conv2(x)
-
-        return x
 
 def center_crop(layer, max_height, max_width):
     _, _, h, w = layer.size()
@@ -95,28 +18,97 @@ def center_crop(layer, max_height, max_width):
     return layer[:,:,diffy:(diffy + max_height),diffx:(diffx + max_width)]
 
 
-def up_pad(layer, skip_height, skip_width):
-    _, _, h, w = layer.size()
-    diffy = skip_height - h
-    diffx = skip_width -w
-    return F.pad(layer,[diffx // 2, diffx - diffx // 2,
-                        diffy // 2, diffy - diffy // 2])
+class DecoderStage(nn.Module):
+
+    def __init__(self,
+                 in_planes,
+                 out_planes,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 output_padding=0,
+                 bias=False):
+        super(DecoderStage, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_planes,
+                in_planes//4,
+                1,
+                1,
+                0,
+                bias=bias),
+            nn.BatchNorm2d(in_planes//4),
+            nn.ReLU(inplace=True))
+        self.tp_conv = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_planes//4,
+                in_planes//4,
+                kernel_size,
+                stride,
+                padding,
+                output_padding,
+                bias=bias),
+            nn.BatchNorm2d(in_planes//4),
+            nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_planes//4,
+                out_planes,
+                1,
+                1,
+                0,
+                bias=bias),
+            nn.BatchNorm2d(out_planes),
+            nn.ReLU(inplace=True))
+
+    def forward(self, x_high_level, x_low_level):
+        x = self.conv1(x_high_level)
+        x = self.tp_conv(x)
+
+        x = center_crop(x, x_low_level.size()[2], x_low_level.size()[3])
+
+        x = self.conv2(x)
+
+        return x
 
 
-class LinkNetImprove(nn.Module):
+class LinkNetHead(nn.Module):
     """
-    Generate Model Architecture
-    """
+    LinkNet head block.
 
-    def __init__(self, classes=19):
-        """
-        Model initialization
-        :param x_n: number of input neurons
-        :type x_n: int
-        """
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    bias : bool
+        Whether the layer uses a bias vector.
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 bias):
+        super(ENetUpBlock, self).__init__()
+        self.conv = conv1x1_block(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            bias=bias,
+            activation=None)
+        self.unpool = nn.MaxUnpool2d(kernel_size=2)
+
+    def forward(self, x, max_indices):
+        x = self.conv(x)
+        x = self.unpool(x, max_indices)
+        return x
+
+
+class LinkNet(nn.Module):
+
+    def __init__(self, num_classes=19):
         super().__init__()
 
-        base = resnet.resnet18(pretrained=True)
+        base = resnet.resnet18(pretrained=False)
 
         self.in_block = nn.Sequential(
             base.conv1,
@@ -130,20 +122,21 @@ class LinkNetImprove(nn.Module):
         self.encoder3 = base.layer3
         self.encoder4 = base.layer4
 
-        self.decoder1 = Decoder(64, 64, 3, 1, 1, 0)
-        self.decoder2 = Decoder(128, 64, 3, 2, 1, 1)
-        self.decoder3 = Decoder(256, 128, 3, 2, 1, 1)
-        self.decoder4 = Decoder(512, 256, 3, 2, 1, 1)
+        self.decoder1 = DecoderStage(64, 64, 3, 1, 1, 0)
+        self.decoder2 = DecoderStage(128, 64, 3, 2, 1, 1)
+        self.decoder3 = DecoderStage(256, 128, 3, 2, 1, 1)
+        self.decoder4 = DecoderStage(512, 256, 3, 2, 1, 1)
 
         # Classifier
-        self.tp_conv1 = nn.Sequential(nn.ConvTranspose2d(64, 32, 3, 2, 1, 1),
-                                      nn.BatchNorm2d(32),
-                                      nn.ReLU(inplace=True),)
-        self.conv2 = nn.Sequential(nn.Conv2d(32, 32, 3, 1, 1),
-                                nn.BatchNorm2d(32),
-                                nn.ReLU(inplace=True),)
-        self.tp_conv2 = nn.ConvTranspose2d(32, classes, 2, 2, 0)
-
+        self.tp_conv1 = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, 3, 2, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),)
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 32, 3, 1, 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),)
+        self.tp_conv2 = nn.ConvTranspose2d(32, num_classes, 2, 2, 0)
 
     def forward(self, x):
         # Initial block
@@ -167,78 +160,6 @@ class LinkNetImprove(nn.Module):
         y = self.tp_conv2(y)
 
         return y
-
-
-class LinkNet(nn.Module):
-    """
-    Generate model architecture
-    """
-
-    def __init__(self, num_classes=19):
-        """
-        Model initialization
-        :param x_n: number of input neurons
-        :type x_n: int
-        """
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 64, 7, 2, 3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(3, 2, 1)
-
-        self.encoder1 = Encoder(64, 64, 3, 1, 1)
-        self.encoder2 = Encoder(64, 128, 3, 2, 1)
-        self.encoder3 = Encoder(128, 256, 3, 2, 1)
-        self.encoder4 = Encoder(256, 512, 3, 2, 1)
-
-
-        self.decoder4 = Decoder(512, 256, 3, 2, 1, 1)
-        self.decoder3 = Decoder(256, 128, 3, 2, 1, 1)
-        self.decoder2 = Decoder(128, 64, 3, 2, 1, 1)
-        self.decoder1 = Decoder(64, 64, 3, 1, 1, 0)
-
-
-        # Classifier
-        self.tp_conv1 = nn.Sequential(nn.ConvTranspose2d(64, 32, 3, 2, 1, 1),
-                                      nn.BatchNorm2d(32),
-                                      nn.ReLU(inplace=True),)
-        self.conv2 = nn.Sequential(nn.Conv2d(32, 32, 3, 1, 1),
-                                nn.BatchNorm2d(32),
-                                nn.ReLU(inplace=True),)
-        self.tp_conv2 = nn.ConvTranspose2d(32, num_classes, 2, 2, 0)
-
-    def forward(self, x):
-        # Initial block
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        # Encoder blocks
-        e1 = self.encoder1(x)
-        e2 = self.encoder2(e1)
-        e3 = self.encoder3(e2)
-        e4 = self.encoder4(e3)
-
-        # Decoder blocks
-        d4 = e3 + self.decoder4(e4, e3)
-        d3 = e2 + self.decoder3(d4, e2)
-        d2 = e1 + self.decoder2(d3, e1)
-        d1 = x + self.decoder1(d2, x)
-
-        # Classifier
-        y = self.tp_conv1(d1)
-        y = self.conv2(y)
-        y = self.tp_conv2(y)
-
-
-        return y
-
-# """print layers and params of network"""
-# if __name__ == '__main__':
-#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-#     model = LinkNet(num_classes=11).to(device)
-#     summary(model,(3,512,1024))
 
 
 def oth_linknet_cityscapes(num_classes=19, pretrained=False, **kwargs):
@@ -266,10 +187,10 @@ def _test():
 
     for model in models:
 
-        from torchsummary import summary
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        net = LinkNet(num_classes=19).to(device)
-        summary(net, (3, 512, 1024))
+        # from torchsummary import summary
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # net = LinkNet(num_classes=19).to(device)
+        # summary(net, (3, 512, 1024))
 
         net = model(pretrained=pretrained)
 

638f13f712531cde03d8384ef08e6370f74dc074
osmr <osemery@gmail.com>
2021-02-15 16:55:20 +0300

    Work on ESPNet, 4

--- pytorch/pytorchcv/models/others/_espnet.py
+++ pytorch/pytorchcv/models/others/_espnet.py
@@ -6,23 +6,129 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from common import NormActivation, conv1x1, conv3x3, conv1x1_block, conv3x3_block, depthwise_conv3x3, SEBlock,\
     Concurrent, DualPathSequential, InterpolationBlock
 
 
-class DownSamplerB(nn.Module):
+class HierarchicalConcurrent(nn.Sequential):
+    """
+    A container for hierarchical concatenation of modules on the base of the sequential container.
+
+    Parameters:
+    ----------
+    axis : int, default 1
+        The axis on which to concatenate the outputs.
+    """
+    def __init__(self,
+                 exclude_first=False,
+                 axis=1):
+        super(HierarchicalConcurrent, self).__init__()
+        self.exclude_first = exclude_first
+        self.axis = axis
+
+    def forward(self, x):
+        out = []
+        y_prev = None
+        for i, module in enumerate(self._modules.values()):
+            y = module(x)
+            if y_prev is not None:
+                y += y_prev
+            out.append(y)
+            if (not self.exclude_first) or (i > 0):
+                y_prev = y
+        out = torch.cat(tuple(out), dim=self.axis)
+        return out
+
+
+class ESPBlock(nn.Module):
+    """
+    ESPNet block.
+
+    Parameters:
+    ----------
+    in_channels : int
+        Number of input channels.
+    out_channels : int
+        Number of output channels.
+    """
     def __init__(self,
                  in_channels,
                  out_channels,
+                 downsample,
+                 residual,
+                 bn_eps):
+        super(ESPBlock, self).__init__()
+        self.residual = residual
+        dilations = [1, 2, 4, 8, 16]
+        num_branches = len(dilations)
+        mid_channels = out_channels // num_branches
+        extra_mid_channels = out_channels - (num_branches - 1) * mid_channels
+
+        if downsample:
+            self.reduce_conv = conv3x3(
+                in_channels=in_channels,
+                out_channels=mid_channels,
+                stride=2)
+        else:
+            self.reduce_conv = conv1x1(
+                in_channels=in_channels,
+                out_channels=mid_channels)
+
+        self.branches = HierarchicalConcurrent(exclude_first=True)
+        for i in range(num_branches):
+            out_channels_i = extra_mid_channels if i == 0 else mid_channels
+            self.branches.add_module("branch{}".format(i + 1), conv3x3(
+                in_channels=mid_channels,
+                out_channels=out_channels_i,
+                padding=dilations[i],
+                dilation=dilations[i]))
+
+        self.norm_activ = NormActivation(
+            in_channels=out_channels,
+            bn_eps=bn_eps,
+            activation=(lambda: nn.PReLU(out_channels)))
+
+    def forward(self, x):
+        y = self.reduce_conv(x)
+        y = self.branches(y)
+        if self.residual:
+            y = y + x
+        y = self.norm_activ(y)
+        return y
+
+
+class ESPBlock1(nn.Module):
+    '''
+    This class defines the ESP block, which is based on the following principle
+        Reduce ---> Split ---> Transform --> Merge
+        :param in_channels: number of input channels
+        :param out_channels: number of output channels
+        :param residual: if true, add a residual connection through identity operation. You can use projection too as
+                in ResNet paper, but we avoid to use it if the dimensions are not the same because we do not want to
+                increase the module complexity
+    '''
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 downsample,
+                 residual,
                  bn_eps):
         super().__init__()
+        self.residual = residual
+
         n = int(out_channels / 5)
         n1 = out_channels - 4 * n
-        self.c1 = conv3x3(
-            in_channels=in_channels,
-            out_channels=n,
-            stride=2)
+
+        if downsample:
+            self.c1 = conv3x3(
+                in_channels=in_channels,
+                out_channels=n,
+                stride=2)
+        else:
+            self.c1 = conv1x1(
+                in_channels=in_channels,
+                out_channels=n)
+
         self.d1 = conv3x3(
             in_channels=n,
             out_channels=n1,
@@ -54,85 +160,12 @@ class DownSamplerB(nn.Module):
             activation=(lambda: nn.PReLU(out_channels)))
 
     def forward(self, x):
-        x = self.c1(x)
-        d1 = self.d1(x)
-        d2 = self.d2(x)
-        d4 = self.d4(x)
-        d8 = self.d8(x)
-        d16 = self.d16(x)
-         
-        # Using hierarchical feature fusion (HFF) to ease the gridding artifacts which is introduced 
-        # by the large effective receptive filed of the ESP module 
-        add1 = d2
-        add2 = add1 + d4
-        add3 = add2 + d8
-        add4 = add3 + d16
-
-        x = torch.cat([d1, add1, add2, add3, add4], 1)
-        x = self.morm_activ(x)
-        return x
-
-
-class DilatedParllelResidualBlockB(nn.Module):
-    '''
-    This class defines the ESP block, which is based on the following principle
-        Reduce ---> Split ---> Transform --> Merge
-        :param in_channels: number of input channels
-        :param out_channels: number of output channels
-        :param add: if true, add a residual connection through identity operation. You can use projection too as
-                in ResNet paper, but we avoid to use it if the dimensions are not the same because we do not want to
-                increase the module complexity
-    '''
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 add,
-                 bn_eps):
-        super().__init__()
-        self.add = add
-        n = int(out_channels / 5)  #K=5,
-        n1 = out_channels - 4 * n  #(N-(K-1)INT(N/K)) for dilation rate of 2^0, for producing an output feature map of channel=nOut
-
-        self.c1 = conv1x1(
-            in_channels=in_channels,
-            out_channels=n)  #the point-wise convolutions with 1x1 help in reducing the computation, channel=c
-
-        #K=5, dilation rate: 2^{k-1},k={1,2,3,...,K}
-        self.d1 = conv3x3(
-            in_channels=n,
-            out_channels=n1,
-            padding=1,
-            dilation=1)  # dilation rate of 2^0
-        self.d2 = conv3x3(
-            in_channels=n,
-            out_channels=n,
-            padding=2,
-            dilation=2)  # dilation rate of 2^1
-        self.d4 = conv3x3(
-            in_channels=n,
-            out_channels=n,
-            padding=4,
-            dilation=4)  # dilation rate of 2^2
-        self.d8 = conv3x3(
-            in_channels=n,
-            out_channels=n,
-            padding=8,
-            dilation=8)  # dilation rate of 2^3
-        self.d16 = conv3x3(
-            in_channels=n,
-            out_channels=n,
-            padding=16,
-            dilation=16)  # dilation rate of 2^4
-        self.morm_activ = NormActivation(
-            in_channels=out_channels,
-            bn_eps=bn_eps,
-            activation=(lambda: nn.PReLU(out_channels)))
-
-    def forward(self, x):
-        identity = x
+        if self.residual:
+            identity = x
 
         # reduce
         x = self.c1(x)
+
         # split and transform
         d1 = self.d1(x)
         d2 = self.d2(x)
@@ -151,7 +184,7 @@ class DilatedParllelResidualBlockB(nn.Module):
         x = torch.cat([d1, add1, add2, add3, add4], 1)
 
         # if residual version
-        if self.add:
+        if self.residual:
             x = x + identity
         x = self.morm_activ(x)
         return x
@@ -188,6 +221,55 @@ class InputProjectionA(nn.Module):
         return x
 
 
+class ESPNetStage(nn.Module):
+    def __init__(self,
+                 x_in_channels,
+                 x_out_channels,
+                 y_in_channels,
+                 y_out_channels,
+                 layers,
+                 bn_eps):
+        super().__init__()
+        self.use_x = (x_out_channels > 0)
+
+        in_channels = x_in_channels + y_in_channels
+        out_channels = x_out_channels + y_out_channels
+        y_mid_channels = y_out_channels // 2
+
+        self.level3_0 = ESPBlock(
+            in_channels=in_channels,
+            out_channels=y_mid_channels,
+            downsample=True,
+            residual=False,
+            bn_eps=bn_eps)
+        self.level3 = nn.ModuleList()
+        for i in range(0, layers):
+            self.level3.append(ESPBlock(
+                in_channels=y_mid_channels,
+                out_channels=y_mid_channels,
+                downsample=False,
+                residual=True,
+                bn_eps=bn_eps))
+        self.norm_activ = NormActivation(
+            in_channels=out_channels,
+            bn_eps=bn_eps,
+            activation=(lambda: nn.PReLU(out_channels)))
+
+    def forward(self, y, x=None):
+        output2_0 = self.level3_0(y)
+        for i, layer in enumerate(self.level3):
+            if i == 0:
+                output2 = layer(output2_0)
+            else:
+                output2 = layer(output2)
+
+        y = torch.cat((output2_0, output2), dim=1)
+        if self.use_x:
+            y = torch.cat((y, x), dim=1)
+        y = self.norm_activ(y)
+        return y
+
+
 class ESPNetC(nn.Module):
     '''
     This class defines the ESPNet-C network in the paper
@@ -214,43 +296,64 @@ class ESPNetC(nn.Module):
             in_channels=(16 + 3),
             bn_eps=bn_eps,
             activation=(lambda: nn.PReLU(16 + 3)))
-        self.level2_0 = DownSamplerB(
-            in_channels=(16 + 3),
-            out_channels=64,
-            bn_eps=bn_eps)  # Downsample Block, feature map size divided 2,    1/4
 
-        self.level2 = nn.ModuleList()
-        for i in range(0, p):
-            self.level2.append(DilatedParllelResidualBlockB(
-                in_channels=64,
-                out_channels=64,
-                add=True,
-                bn_eps=bn_eps))  # ESP block
-        self.b2 = NormActivation(
-            in_channels=(128 + 3),
-            bn_eps=bn_eps,
-            activation=(lambda: nn.PReLU(128 + 3)))
+        self.level2_ = ESPNetStage(
+            x_in_channels=3,
+            x_out_channels=3,
+            y_in_channels=16,
+            y_out_channels=128,
+            layers=p,
+            bn_eps=bn_eps)
 
-        self.level3_0 = DownSamplerB(
-            in_channels=128 + 3,
-            out_channels=128,
-            bn_eps=bn_eps)  # Downsample Block, feature map size divided 2,   1/8
-        self.level3 = nn.ModuleList()
-        for i in range(0, q):
-            self.level3.append(DilatedParllelResidualBlockB(
-                in_channels=128,
-                out_channels=128,
-                add=True,
-                bn_eps=bn_eps))  # ESPblock
+        # self.level2_0 = DilatedParllelResidualBlockB(
+        #     in_channels=(16 + 3),
+        #     out_channels=64,
+        #     downsample=True,
+        #     residual=False,
+        #     bn_eps=bn_eps)  # Downsample Block, feature map size divided 2,    1/4
+        # self.level2 = nn.ModuleList()
+        # for i in range(0, p):
+        #     self.level2.append(DilatedParllelResidualBlockB(
+        #         in_channels=64,
+        #         out_channels=64,
+        #         downsample=False,
+        #         residual=True,
+        #         bn_eps=bn_eps))  # ESP block
+        # self.b2 = NormActivation(
+        #     in_channels=(128 + 3),
+        #     bn_eps=bn_eps,
+        #     activation=(lambda: nn.PReLU(128 + 3)))
 
-        self.b3 = NormActivation(
-            in_channels=256,
-            bn_eps=bn_eps,
-            activation=(lambda: nn.PReLU(256)))
+        self.level3_ = ESPNetStage(
+            x_in_channels=3,
+            x_out_channels=0,
+            y_in_channels=128,
+            y_out_channels=256,
+            layers=q,
+            bn_eps=bn_eps)
 
-        self.classifier = conv1x1(
+        # self.level3_0 = DownSamplerB(
+        #     in_channels=128 + 3,
+        #     out_channels=128,
+        #     bn_eps=bn_eps)  # Downsample Block, feature map size divided 2,   1/8
+        # self.level3 = nn.ModuleList()
+        # for i in range(0, q):
+        #     self.level3.append(DilatedParllelResidualBlockB(
+        #         in_channels=128,
+        #         out_channels=128,
+        #         add=True,
+        #         bn_eps=bn_eps))  # ESPblock
+        # self.b3 = NormActivation(
+        #     in_channels=256,
+        #     bn_eps=bn_eps,
+        #     activation=(lambda: nn.PReLU(256)))
+
+        self.head = conv1x1(
             in_channels=256,
             out_channels=num_classes)
+        self.up = InterpolationBlock(
+            scale_factor=8,
+            align_corners=False)
 
     def forward(self, x):
         output0 = self.level1(x)
@@ -258,29 +361,13 @@ class ESPNetC(nn.Module):
         inp2 = self.sample2(x)
 
         output0_cat = self.b1(torch.cat([output0, inp1], 1))
-        output1_0 = self.level2_0(output0_cat) # down-sampled
-        
-        for i, layer in enumerate(self.level2):
-            if i==0:
-                output1 = layer(output1_0)
-            else:
-                output1 = layer(output1)
 
-        output1_cat = self.b2(torch.cat([output1,  output1_0, inp2], 1))
+        output1_cat = self.level2_(output0_cat, inp2)
+        y = self.level3_(output1_cat)
 
-        output2_0 = self.level3_0(output1_cat) # down-sampled
-        for i, layer in enumerate(self.level3):
-            if i==0:
-                output2 = layer(output2_0)
-            else:
-                output2 = layer(output2)
-
-        output2_cat = self.b3(torch.cat([output2_0, output2], 1))
-
-        classifier = self.classifier(output2_cat)
-
-        out = F.upsample(classifier, x.size()[2:], mode='bilinear')   #Upsample score map, factor=8
-        return out
+        y = self.head(y)
+        y = self.up(y)
+        return y
 
 
 class ESPNet(nn.Module):
@@ -297,14 +384,14 @@ class ESPNet(nn.Module):
                  q=3,
                  bn_eps=1e-03):
         super().__init__()
-        encoder = ESPNetC(
+        self.encoder = ESPNetC(
             num_classes=num_classes,
             p=p,
             q=q,
             bn_eps=bn_eps)
         # load the encoder modules
         self.en_modules = []
-        for i, m in enumerate(encoder.children()):
+        for i, m in enumerate(self.encoder.children()):
             self.en_modules.append(m)
 
         # light-weight decoder
@@ -331,10 +418,11 @@ class ESPNet(nn.Module):
                 in_channels=(2 * num_classes),
                 bn_eps=bn_eps,
                 activation=(lambda: nn.PReLU(2 * num_classes))),
-            DilatedParllelResidualBlockB(
+            ESPBlock(
                 in_channels=(2 * num_classes),
                 out_channels=num_classes,
-                add=False,
+                downsample=False,
+                residual=False,
                 bn_eps=bn_eps))
 
         self.up_l2 = nn.Sequential(
@@ -421,8 +509,8 @@ def _test():
     classes = 19
 
     models = [
-        # espnet_cityscapes,
         espnetc_cityscapes,
+        # espnet_cityscapes,
     ]
 
     for model in models:
